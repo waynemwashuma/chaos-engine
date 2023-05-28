e977088516cabbde91b4d4ac671c72a181b4db0d
mwashumawayne@gmail.com <mwashumawayne@gmail.com>
2023-05-28 11:56:46 +0300

    New:Added documentation to the Entity class

--- src/manager/entity.js
+++ src/manager/entity.js
@@ -3,6 +3,12 @@ import { Body, AABBox } from "../physics/index.js"
 import { BodyMesh } from "../render/index.js"
 import { Transform, Movable } from "/src/index.js"
 
+/**
+ * This is a container to hold components and event handlers.
+ * 
+ * @class
+ * @public
+ */
 class Entity {
   _components = {}
   _global = null
@@ -15,6 +21,9 @@ class Entity {
   get CHAOS_CLASSNAME() {
     return this.constructor.name.toLowerCase()
   }
+  /**
+   * Removes all components and handlers from an entity while removing it from its manager
+   */
   destroy() {
     this.removeSelf()
     for (let k in this._components) {
@@ -28,26 +37,38 @@ class Entity {
     }
 
   }
+  /**
+   * Removes an entity and its components from its manager whilst retaining its components and handlers
+   */
   removeSelf() {
     if (this._global) this._global.remove(this)
     this.active = false
     this._global = null
   }
+  /**
+   * Removes all components of an entity from its manager but keeps the entity inside the manager.
+   * This is an internal function so no need on your part to use it.
+   */
   removeComponents() {
     if (this._global === void 0) return
     for (var k in this._components) {
       this._global.removeComponent(k, this._components[k])
     }
   }
+  /**
+   * Gets the current manager of an entity
+   * 
+   * @returns {Manager}
+   */
   get manager() {
     return this._global
   }
-  set parent(x) {
-    this._parent = x
-  }
-  get parent() {
-    return this._parent
-  }
+  /**
+   * Adds a component into an entity
+   * 
+   * @param {String} n Name of the component.
+   * @param {Component} c The component to add.
+   */
   attach(n, c) {
     this._components[n] = c
     if (this.manager) {
@@ -56,35 +77,92 @@ class Entity {
     }
     return this
   }
+  /**
+   * Removes a component from an entity.
+   * 
+   * @param {String} n Name pf the component
+   */
   remove(n) {
     this._global.removeComponent(n, this._components[n])
     delete this._components[n]
+    return this
   }
+  /**
+   * Registers a function to handle a named event.
+   * 
+   * @param {string} n Name of the event
+   * @param {function} h The function to be called when an event is fired.
+   */
   register(n, h) {
     this._handlers[n] = h
   }
+  /**
+   * Removes an event handler function of the given name
+   * 
+   * @param {string} n Name of the event
+   */
   unregister(n) {
     if (!(n in this._handlers)) return
     delete this._handlers[n]
   }
+  /**
+   * Returns an event handler which can be fired during an event
+   * 
+   * @@param {string} n Name of the event
+   */
   getHandler(n) {
     return this._handlers[n]
   }
+  /**
+   * Returns the named component.
+   * 
+   * @param {string} n Name of the component.
+   * @returns {Component | undefined }
+   */
   get(n) {
     return this._components[n]
   }
+  /**
+   * Used to check if the component exists in an entity
+   * 
+   * @param {string} n Name of the component.
+   * @returns {boolean}
+   */
   has(n) {
     return n in this._components
   }
+  /**
+   * Adds a tag into an entity.
+   * 
+   * @param {string} n The tag to be added
+   */
   addTag(n) {
     this._tags.add(n)
   }
+  /**
+   * Removes a tag from an entity.
+   * 
+   * @param {string} n The tag to be added
+   */
   removeTag(n) {
     this._tags.delete(n)
   }
+  /**
+   * Checks if a tag exists in an entity.
+   * 
+   * @param {string} n The tag to be added
+   * @returns {boolean}
+   */
   hasTag(n) {
     return this._tags.has(n)
   }
+  /**
+   * Initializes the components within an entity and marks it as active.
+   * It is called by an instance of a game manager so no need to call it manually
+   * 
+   * @private
+   * @param {Manager} global
+   */
   init(global) {
     this._global = global
     this.active = true
@@ -93,25 +171,27 @@ class Entity {
       global.addComponent(k, this._components[k])
     }
   }
-
-  update(dt) {
-    for (let k in this._components) {
-      if (k == "mesh" || k == "body") continue
-      this._components[k].update(dt)
-    }
-  }
-  static DefaultPosition = new Vector()
+  /**
+   * A helper function to create a new Entity with transform,movable and bounds components.
+   */
   static Default(x, y, a) {
     return new Entity()
       .attach("transform", new Transform(x, y, a))
       .attach("movable", new Movable())
       .attach("bounds", new AABBox())
   }
+  /**
+   * Search an entity's manager for entities in a given bound.
+   * 
+   * @param {Object} bound the region to search entitities in.
+   * @param {[]} [target=[]] An array to store results in.
+   * @returns {array<Entity>}
+   */
   query(bound, target = []) {
     return this._global.query(bound, target)
   }
 }
-
+let r = new Entity()
 export {
   Entity
 }
