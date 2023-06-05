34ff29d21d45a39f5dea27b966356f6a5cfa6088
mwashumawayne@gmail.com <mwashumawayne@gmail.com>
2023-06-05 21:53:00 +0300

    Add:new methods to Composite,needs more work.

--- src/physics/bodies/composite.js
+++ src/physics/bodies/composite.js
@@ -1,4 +1,4 @@
-import { Utils } from "/src/utils/index.js"
+import { Utils, Vector } from "/src/utils/index.js"
 import { ObjType } from "../settings.js"
 
 
@@ -26,6 +26,99 @@ class Composite {
   }
   update() {
     this.lastPosition.copy(this.position)
+  }
+
+  get acceleration() {
+    let acceleration= new Vector()
+    for (var i = 0; i < this.bodies.length; i++) {
+      acceleration.copy(this.bodies[i].acceleration)
+    }
+    return acceleration.divide(this.bodies.length)
+  }
+  set acceleration(x) {
+    for (var i = 0; i < this.bodies.length; i++) {
+      this.bodies[i].acceleration = x
+    }
+  }
+  get velocity() {
+    let velocity = new Vector()
+    
+    for (var i = 0; i < this.bodies.length; i++) {
+      velocity.add(this.bodies[i].velocity)
+    }
+    return velocity.divide(this.bodies.length)
+  }
+  set velocity(x) {
+    for (var i = 0; i < this.bodies.length; i++) {
+      this.bodies[i].velocity.copy(x)
+    }
+  }
+
+  set angle(angle) {
+    for (var i = 0; i < this.bodies.length; i++) {
+      this.bodies[i].angle = x
+    }
+  }
+  get angle() {
+    let angle = 0
+    for (var i = 0; i < this.bodies.length; i++) {
+      angle += this.bodies[i].angle
+    }
+  }
+  set mass(x) {
+    for (var i = 0; i < this.bodies.length; i++) {
+      this.bodies[i].mass = x
+    }
+  }
+  get mass() {
+    let mass = 0
+    for (var i = 0; i < this.bodies.length; i++) {
+      mass += this.bodies[i].mass
+    }
+    return mass
+  }
+  set density(x) {
+    let area = 0
+    for (var i = 0; i < this.bodies.length; i++) {
+      this.bodies[i].density = x
+    }
+  }
+  get density() {
+    let density = 0
+    for (var i = 0; i < this.bodies.length; i++) {
+      density += this.bodies[i].density
+    }
+    return density / this.bodies.length
+  }
+  get position() {
+    let position = new Vector()
+    for (var i = 0; i < this.shapes.length; i++) {
+      position.add(this.bodies[i].position)
+    }
+    return position
+  }
+  set position(x) {
+    let dp = x.clone().sub(this.position)
+    for (var i = 0; i < this.bodies.length; i++) {
+      this.bodies[i].position.add(dp)
+    }
+  }
+  set orientation(r) {
+    for (var i = 0; i < this.bodies.length; i++) {
+      this.bodies[i].orientation.copy(r)
+    }
+  }
+  get angularVelocity() {
+    let ang = 0
+    for (var i = 0; i < this.bodies.length; i++) {
+      ang += this.bodies[i].angularVelocity
+    }
+    return ang
+  }
+  set angularVelocity(x) {
+    for (var i = 0; i < this.bodies.length; i++) {
+      this.bodies[i].angularVelocity = x
+    }
   }
 }
 Utils.inheritComponent(Composite)
