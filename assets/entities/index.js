7a9d4a45bc71e9d940a37910f69deb5916f0daaf
mwashumawayne@gmail.com <mwashumawayne@gmail.com>
2023-06-06 11:55:30 +0300

    Remove:assets are not required for demos to work

--- assets/entities/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./box.js"
-export * from "./Wall.js"
-export * from"./ball.js"
-export * from "./raycastor.js"
--- assets/splines/linear.js
+++
@@ -1,9 +0,0 @@
-import {Vector} from "../index.js"
-
-function Linear(p1,p2,t){
-  return Vector.lerp(p1,p2,t)
-}
-
-export{
-  Linear
-}
