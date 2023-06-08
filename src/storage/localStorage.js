84246023c4a68b73bc7692ee8e242d05f2f58a8b
mwashumawayne@gmail.com <mwashumawayne@gmail.com>
2023-06-08 19:42:19 +0300

    New:Documentation of LocalStorage

--- src/storage/localStorage.js
+++ src/storage/localStorage.js
@@ -1,12 +1,30 @@
+/**
+ * This provides permanent storage
+*/
+
 export const Storage = {
+  /**
+   * Adds a value to local storage
+   * 
+   * @param {string} k 
+   * @param {any} v
+  */
   set(k,v) {
     let json = JSON.stringify(v)
     localStorage.setItem(k,v)
   },
+  /**
+   * Gets a value from local storage by its key.
+   * 
+   * @param {string} k
+  */
   get(k) {
     let json = localStorage.getItem(k)
     return JSON.parse(json)
   },
+  /**
+   * Removes everything from local storage 
+  */
   clear() {
     localStorage.clear()
   }
