9e120979df6a6ce597bb1fe5242c3226a13a916f
mwashumawayne@gmail.com <mwashumawayne@gmail.com>
2023-06-09 01:17:30 +0300

    New: Documentation of Broadphase class

--- src/physics/broadphases/broadphase.js
+++ src/physics/broadphases/broadphase.js
@@ -1,6 +1,19 @@
+/**
+ * This is an abstract class that extended to classes that are used to filter out unnecessary collision checks to boost performance.
+ * 
+ * @abstract
+ * @see QuadtreeBroadphase
+ * @see GridBroadphase
+ * @see AABBBroadphase
+ */
 class Broadphase {
+  /**
+   * Checks to see if two bodies can proceed to have their bounding boxes checked 
+   * 
+   * @param {Body} a
+   * @param {Body} b
+   */
   canCollide(a, b) {
-    
     if (a.mass == 0 && b.mass == 0)
       return false
     if (
@@ -9,16 +22,33 @@ class Broadphase {
     ) return false
     if (a.mask.layer && b.mask.layer && a.mask.layer !== b.mask.layer)
       return false
-    if(a.sleeping && b.sleeping) return false
+    if (a.sleeping && b.sleeping) return false
     //console.log(a.mass,b.mass);
     return true
   }
-  insert() {}
-  remove() {}
-  traverse(func) {}
-  draw(ctx) {}
-  update(obj) {}
+  /**
+   * Adds a body to the broadphase
+   * 
+   * @param {Body} obj
+   */
+  insert(obj) {}
+  /**
+   * Removes a body from the broadphase
+   * 
+   * @param {Body} obj
+   */
   remove(obj) {}
+
+  /**
+   * Renders a representation of a broadphase
+  */
+  draw(ctx) {}
+  /**
+   * Updates the internals of the broadphase if needed.
+   * 
+   * @param {array<Body>} bodies
+  */
+  update(bodies) {}
 }
 
 export {
