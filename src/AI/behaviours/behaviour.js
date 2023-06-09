84b4952f036a4e455cd1705c88fe4e9847bfd31d
mwashumawayne@gmail.com <mwashumawayne@gmail.com>
2023-06-09 18:05:43 +0300

    new: behavior class documented

--- src/AI/behaviours/behaviour.js
+++ src/AI/behaviours/behaviour.js
@@ -1,9 +1,20 @@
+/**
+ * Base class for implementing customized behaviours.
+ * 
+ * @abstract
+*/
+
 class Behaviour {
   constructor() {
     this.active = true
   }
+  /**
+   * Sets up a behavior to work on an agent.
+  */
   init(){}
-  update() {}
+  /**
+   * Calculates the amount of force required to satisfy a behavior
+  */
   calc() {}
 }
 export {
