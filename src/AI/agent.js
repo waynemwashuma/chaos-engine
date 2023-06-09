9e889b0646099295df7600c610717996a85bc79b
mwashumawayne@gmail.com <mwashumawayne@gmail.com>
2023-06-09 17:42:23 +0300

    New:Documented Agent

--- src/AI/agent.js
+++ src/AI/agent.js
@@ -1,17 +1,52 @@
-import {Utils} from "../utils/index.js"
-import {BehaviourManager} from "./behaviourManager.js"
+import { Utils } from "../utils/index.js"
+import { BehaviourManager } from "./behaviourManager.js"
 
+/**
+ * This is a component class used to add AI behavior to an entity.
+ * 
+ * @implements Component
+ */
 class Agent {
+  /**
+   * The velocity of the entity.
+   * 
+   * @package
+   * @type Vector
+   */
   velocity = null
+  /**
+   * The rotation of the entity.
+   * 
+   * @package
+   * @type Angle
+   */
   rotation = null
+  /**
+   * The maximum speed of the agent in pixels per second.
+   * 
+   * @type number
+   */
   maxSpeed = 20
+  /**
+   * Maximum rotation of the agent in radians per second
+   * Not yet implemented.
+  */
   maxTurnRate = 5
+  /**
+   * 
+   * @private
+   * @type BehaviourManager
+  */
   behaviours = new BehaviourManager()
-  init(parent){
+  /**
+   * @inheritdoc
+   * @param {parent}
+   */
+  init(parent) {
     this.parent = parent
-    this.requires("transform","movable")
+    this.requires("transform", "movable")
     let move = this.get("movable"),
-    transform = this.get("transform")
+      transform = this.get("transform")
     this.velocity = move.velocity
     this.rotation = move.rotation
     this.position = transform.position
@@ -19,13 +54,27 @@ class Agent {
     this.acceleration = move.acceleration
     this.behaviours.init(this)
   }
-  add(behaviour){
+  /**
+   * Adds a behavior to the agent.
+   * 
+   * @param {Behavior} behaviour
+   */
+  add(behaviour) {
     this.behaviours.add(behaviour)
   }
+  /**
+   * Removes a behavior to the agent.
+   * 
+   * @param {Behavior} behaviour
+   */
   remove(behaviour) {
     this.behaviours.remove(behaviour)
   }
-  update(inv_dt){
+  /**
+   * @inheritdoc
+   * @param {number} inv_dt Inverse of delta time i.e frameRate.
+   */
+  update(inv_dt) {
     this.behaviours.update(inv_dt)
   }
 }
