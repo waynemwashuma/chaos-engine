b0146c1a465302594640c15d3aec3c7881822875
mwashumawayne@gmail.com <mwashumawayne@gmail.com>
2023-06-09 16:48:33 +0300

    New:Documentation of Audio

--- src/audio/audio.js
+++ src/audio/audio.js
@@ -1,5 +1,7 @@
 import { Utils } from "../utils/index.js"
-
+/**
+ * This class is responsible for playing a singular sound.
+*/
 class Sfx {
   /**
    * @private
@@ -16,7 +18,15 @@ class Sfx {
    * @type {function}
    */
   _onended = null
+  /**
+   * @private
+   * @type {AudioNode}
+  */
   _destination = null
+  /**
+   * @private
+   * @type {number}
+  */
   _playingOffset = 0
   offset = 0
   loop = false
@@ -39,11 +49,17 @@ class Sfx {
   set onended(x) {
     this._onended = x
   }
-  start() {
-    this._playingOffset = this.offset
-    this.play()
-  }
+  /**
+   * Plays an sfx from the beginning.
+  */
   play() {
+    this._playingOffset = this.offset
+    this.resume()
+  }
+  /**
+   * Continues playing an sfx from where it was paused.
+  */
+  resume() {
     this._source = this.ctx.createBufferSource()
     this._source.buffer = this._soundBuffer
     this._startTime = Date.now()
@@ -51,19 +67,35 @@ class Sfx {
     this._source.start(this.delay, this._playingOffset, this.duration)
     this._source.loop = this.loop
   }
+  /**
+   * Halts playing of an sfx.
+  */
   pause() {
     this._source.stop()
     let time = (Date.now() - this._startTime) / 1000 + this._playingOffset
     this._playingOffset = this.duration <= time ? this.offset : time
   }
+  /**
+   * Disconnects this sfx from its current destination.
+  */
   disconnect() {
     this._source.disconnect()
   }
+  /**
+   * Sets the given audionode to be the output destination of an sfx
+   * 
+   * @param {AudioNode} node
+  */
   connect(node) {
     this._source.disconnect()
+    this._destination = node
     this._source.connect(node)
   }
 }
+
+/**
+ * Manages playing of audio using Web Audio.
+*/
 class AudioHandler {
   ctx = new AudioContext()
   sfx = {}
@@ -105,6 +137,9 @@ class AudioHandler {
         }
       }).catch(err => console.log(err))
   }
+  /**
+   * Loads all audio from the loader.
+  */
   loadFromLoader(loader) {
     for (var n in loader.sfx) {
       let name = n
@@ -118,7 +153,12 @@ class AudioHandler {
       })
     }
   }
-  playMusic(name) {
+  /**
+   * Plays a single audio as the background in a loop throughout the game
+   * 
+   * @param {string} name
+  */
+  playBackgroundMusic(name) {
     this._backname = name
     if (!(name in this.sfx))
       return
@@ -126,6 +166,13 @@ class AudioHandler {
     this._background.connect(this.masterGainNode)
     this._background.play(0, 0, true)
   }
+  /**
+   * Plays a sound only once.
+   * 
+   * @param {string} name Name of audio to play.
+   * @param {number} [offset] Where to start playing the audio.It is in seconds.
+   * @param {number} [duration] how long in seconds will the audio defaults to total duration of the selected audio. 
+  */
   playEffect(name, offset = 0, duration = 0) {
     if (!(name in this.sfx)) {
       this.toplay[name] = 1
@@ -140,23 +187,35 @@ class AudioHandler {
     this.playing.push(s)
     s.play()
   }
-  playAll()
+  /**
+   * Pauses currently playing sounds.
+  */
   pauseAll() {
     this.playing.forEach(sound => {
       sound.stop()
     })
   }
+  /**
+   * Sets the volume to zero.Sounds will continue playing but not be audible.
+  */
   mute() {
     this._mute = this.masterGainNode.gain
     this.masterGainNode.gain = 0
     
   }
+  /**
+   * Restores the volume before it was muted.
+  */
   unmute() {
     this.masterGainNode.gain = this._mute
   }
+  /**
+   * Removes an sfx from the handler and disconnects it from its destination.
+  */
   remove(sfx) {
     let id = this.playing.indexOf(sfx)
     if (id == -1) return
+    sfx.disconnect()
     Utils.removeElement(this.playing, id)
   }
 }
