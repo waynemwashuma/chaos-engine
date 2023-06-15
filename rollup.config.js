5aa2089fc58f9fb1d33da29da063c247de4f3902
mwashumawayne@gmail.com <mwashumawayne@gmail.com>
2023-06-15 12:02:48 +0300

    None:rollup config for build

--- rollup.config.js
+++ rollup.config.js
@@ -1,51 +1,35 @@
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import { terser } from "rollup-plugin-terser";
-import babel from "@rollup/plugin-babel";
-import pkg from "./package.json";
 const input = "src/index.js";
-export default [
-  {
+const name = "chaos"
+
+let banner = `/*
+ * Wayne Mwashuma
+ * {@link https://github.com/waynemwashuma/chaos engine}
+ * @copyright Wayne Mwashuma (@waynemwashuma)
+ * @license MIT
+ */`
+export default [{
     // UMD
     input,
-    plugins: [
-      nodeResolve(),
-      babel({
-        babelHelpers: "bundled",
-      }),
-      terser()
-    ],
+    plugins: [],
     output: {
-      file: `dist/${pkg.name}.min.js`,
+      file: `dist/${name}.umd.js`,
       format: "umd",
       name: "CHAOS",
       esModule: false,
       exports: "named",
       sourcemap: true,
+      banner
     },
-    banner: `/*
- * Wayne Mwashuma
- * {@link https://github.com/waynemwashuma/chaos engine}
- * @copyright Wayne Mwashuma (@waynemwashuma)
- * @license MIT
- */`,
   },
-// ESM and CJS
   {
+    // ESM
     input,
-    //plugins: [nodeResolve()],
-    output: [
-      {
-        dir: "dist/esm",
-        format: "esm",
-        exports: "named",
-        sourcemap: true,
-      },
-      {
-        dir: "dist/cjs",
-        format: "cjs",
-        exports: "named",
-        sourcemap: true,
-      },
-    ],
+    plugins: [],
+    output: {
+      file: `dist/${name}.umd.js`,
+      format: "esm",
+      exports: "named",
+      sourcemap: true,
+    },
   },
 ];
