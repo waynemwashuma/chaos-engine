c34d93cbc88f41e5ed9da52162524024ec8d4422
mwashumawayne@gmail.com <mwashumawayne@gmail.com>
2023-06-16 10:44:14 +0300

    Update:setting groundwork for path

--- src/AI/paths/path.js
+++ src/AI/paths/path.js
@@ -1,4 +1,29 @@
+import {Vector} from "../../utils/index.js"
+
 class Path{
-  waypoints = []
+  _points = []
   lerpfunc = null
+  distFunc = null
+  _index = 0
+  _lerp_t = 0
+  _current = new Vector()
+  loop = false
+  add(point){
+    this._points.push(point)
+    
+    return this
+  }
+  
+  clear(){
+    this._points.length = 0
+		this._index = 0
+
+		return this
+  }
+  remove(){
+    
+  }
+  update(dt){
+    
+  }
 }
