b0189fabd3c8d5448160124f6a39fc9722c6900c
mwashumawayne@gmail.com <mwashumawayne@gmail.com>
2023-06-20 16:07:29 +0300

    Refactor: Arrive behaviour internals changed

--- src/AI/behaviours/arrive.js
+++ src/AI/behaviours/arrive.js
@@ -1,6 +1,8 @@
 import { Behaviour } from "./behaviour.js"
 import { Vector, map, clamp } from "../../utils/index.js"
 
+import {ctx} from "/src/debug.js"
+
 let tmp1 = new Vector(),
   tmp2 = new Vector()
   
@@ -8,20 +10,14 @@ let tmp1 = new Vector(),
  * Not complete.
 */
 class ArriveBehaviour extends Behaviour {
-  maxSpeed = 100
-  maxForce = 200
+  maxSpeed = 1000
+  maxForce = 2000
   arrive = false
   arrivespeed = 1
   radius = 1000
   constructor(pursued) {
     super()
-    let transform = pursued.get("transform")
-    let move = pursued.get("movable")
-    //TODO - Check if the above are undefined
-    this.pursuedPos = transform.position
-    this.pursuedVel = move.velocity
-    this.pursuedRot = move.rotation
-    this.pursuedOrt = transform.orientation
+    this.target = pursued
   }
 
   /**
@@ -29,18 +25,16 @@ class ArriveBehaviour extends Behaviour {
    * @param {Agent} agent
    */
   init(agent) {
-    this.pursuerPos = agent.position
-    this.pursuerVel = agent.velocity
-    this.pursuerRot = agent.rotation
-    this.pursuerOrt = agent.orientation
+    this.position = agent.position
+    this.velocity = agent.velocity
   }
   /**
    * @inheritdoc
    * @param {} target
    */
   calc(target, inv_dt) {
-    let difference = tmp1.copy(this.pursuedPos).sub(this.pursuerPos)
-    let velocity = tmp2.copy(this.pursuerVel)
+    let difference = tmp1.copy(this.target).sub(this.position)
+    let velocity = tmp2.copy(this.velocity)
     let length = difference.magnitude()
 
     if (length < this.radius) {
@@ -52,7 +46,8 @@ class ArriveBehaviour extends Behaviour {
     let steering = difference.sub(velocity).multiply(inv_dt)
 
     steering.clamp(0, this.maxForce)
-    steering.draw(ctx, ...this.pursuerPos)
+    steering.draw(ctx, ...this.position)
+    this.target.draw(ctx)
     target.copy(steering)
   }
 }
