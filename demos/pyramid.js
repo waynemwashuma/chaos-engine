5e65be5fc64fb8fff4a4faa56c347d9c88830211
mwashumawayne@gmail.com <mwashumawayne@gmail.com>
2023-07-05 04:30:03 +0300

    Update: pyramid demo

--- demos/pyramid.js
+++ demos/pyramid.js
@@ -1,7 +1,7 @@
 import { Box, BodySprite, Entity, Vector } from "/dist/chaos.module.js"
 
 export function pyramid(manager) {
-  stackpyramid(200, 300, 50, 50, 3, 5, manager)
+  stackpyramid(200, 100, 50, 50, 3, 5, manager)
   manager.getSystem("world").gravity = 980
 }
 
@@ -12,7 +12,7 @@ function stackpyramid(x, y, w, h, no, spacing, manager) {
     for (var i = 0; i < j; i++) {
       let entity = Entity.Default(dx + w * i, y + (h + spacing) * j)
         .attach("mesh",new BodySprite())
-        .attach("body",new Box(x, h))
+        .attach("body",new Box(w, h))
       manager.add(entity)
     }
   }
