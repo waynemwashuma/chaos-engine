a0b234c0333ff756a0b33a3f3c742cc686fea905
mwashumawayne@gmail.com <mwashumawayne@gmail.com>
2023-07-07 11:49:35 +0300

    None:Updated path resolution of typedef script

--- scripts/typedef.js
+++ scripts/typedef.js
@@ -1,8 +1,8 @@
 const fs = require("fs")
 const path = require("path")
 
-let inputDir1 = path.join(__dirname,"src","typedef")
-let inputDir2 = path.join(__dirname,"dist")
+let inputDir1 = path.resolve(__dirname,"../src","typedef")
+let inputDir2 = path.resolve(__dirname,"../dist")
 let files = fs.readdirSync(inputDir1)
 let outputfiles = fs.readdirSync(inputDir2)
 
