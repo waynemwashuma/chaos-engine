68ad25a51e3ffa108b5f4a9097e40dfbedd7565f
mwashumawayne@gmail.com <mwashumawayne@gmail.com>
2023-08-15 22:47:28 +0300

    Documentation: uodated Material docs

--- src/render/material/material.js
+++ src/render/material/material.js
@@ -4,8 +4,8 @@
 export class Material{
   /**
    * @param {CanvasRenderingContext2D} ctx
-   * @param {number} [dt]
-   * @param {Path2D} path
+   * @param {number} dt
+   * @param {Path2D} [path]
   */
   render(ctx,dt,path){
     throw "Override this method in derived class"
