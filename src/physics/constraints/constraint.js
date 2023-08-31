bf6c28f66f16e55280c016d848cf308b6884adf6
mwashumawayne@gmail.com <mwashumawayne@gmail.com>
2023-08-31 15:45:26 +0300

    [Physics] Serialization method for `Constriant()` #new

--- src/physics/constraints/constraint.js
+++ src/physics/constraints/constraint.js
@@ -63,6 +63,31 @@ class Constraint {
   update(dt) {
     this.behavior(this.body1, this.body2, dt)
   }
+  toJson() {
+    return {
+      body1: this.body1.id,
+      body2: this.body2.id,
+      localA: this.localA.toJson(),
+      localA: this.localB.toJson(),
+      stiffness: this.stiffness,
+      dampening: this.dampening,
+      type:this.CHAOS_OBJ_TYPE
+    }
+  }
+  fromJson(obj, world) {
+    let bod1 = world.getById(obj.body1)
+    let bod2 = world.getById(obj.body2)
+
+    let constraint = new Constraint(
+      bod1,
+      bod2,
+      new Vector().fromJson(obj.localA),
+      new Vector().fromJson(obj.localB)
+    )
+    constraint.stiffness = obj.stiffness
+    constraint.dampening = obj.dampening
+    return constraint
+  }
 }
 export {
   Constraint
