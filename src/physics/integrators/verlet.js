40429763f6131692625d571e3f14f14f7029bf8a
mwashumawayne@gmail.com <mwashumawayne@gmail.com>
2023-10-14 18:44:20 +0300

    [Physics] Fixed `Verlet` intergrator #fix

--- src/physics/integrators/verlet.js
+++ src/physics/integrators/verlet.js
@@ -19,11 +19,11 @@ class VerletSolver {
     body.acceleration.set(0, 0)
     body.velocity.add(acceleration)
     position.add(velocity.multiply(dt)).add(acceleration.multiply(dt))
-    //body.velocity.add(acceleration)
+    body.velocity.add(acceleration)
     body.position = position
     body.angularVelocity += body.angularAcceleration * dt * 0.5
     body.angle += body.angularVelocity * dt
-    //body.angularVelocity += body.angularAcceleration * dt * 0.5
+    body.angularVelocity += body.angularAcceleration * dt * 0.5
   }
 }
 export {
