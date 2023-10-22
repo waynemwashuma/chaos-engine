6b0376d40bb2bf105d11f2b79b15c494007d3272
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2023-10-22 03:23:06 -0700

    [physics] Fixed a collision response of trigon due to rad/deg conversion error #fix

--- src/physics/shapes/triangle.js
+++ src/physics/shapes/triangle.js
@@ -11,17 +11,17 @@ let tmp1 = new Vector(),
  */
 class Triangle extends Shape {
   /**
-   * @param {number} length1 Length of one side.
-   * @param {number} length2 Length of a second side.
+   * @param {number} base Length of one side.
+   * @param {number} height Length of a second side.
    * @param {number} angle The angle between the two sides.
    * @param {Vector} offset Positional offset from the body center.
    * @param {number} offsetAngle Angular offset from the body center.
    * 
    */
-  constructor(length1, length2, angle, offset, offsetAngle) {
-    let l1 = new Vector().set(1, 0).multiply(length1)
-    let l2 = Vector.fromDeg(angle).multiply(length2)
-    let center = tmp1.set((l1.x + l2.x) / 3, (l1.x + l2.x) / 3)
+  constructor(base, height, angle, offset, offsetAngle) {
+    let l1 = new Vector().set(1, 0).multiply(base)
+    let l2 = Vector.fromRad(angle).multiply(height/Math.sin(angle))
+    let center = tmp1.set((l1.x + l2.x) / 3, l2.y / 3)
     super([
       new Vector().sub(center),
       l1.sub(center),
@@ -29,7 +29,7 @@ class Triangle extends Shape {
     ], offset, offsetAngle)
   }
   static calcInertia(mass,base,height,angle){
-    return 0.5 * mass * base * height * (1 - 2/3 * (1 - (Math.cos(2 * angle))/2))
+    return 0.5 * mass * base * height * (1 - 2/3 * (1 - (Math.cos(2 * angle * 180/Math.PI))/2))
   }
 }
 
