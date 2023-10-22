a12e40ff9146e9045c925af7c708aba5947c12fb
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2023-10-22 03:24:09 -0700

    [demos] Fixed triangle example to work with new changes #fix

--- demos/triangle.js
+++ demos/triangle.js
@@ -1,16 +1,16 @@
 import {
-    Entity,
-    Trigon,
-    BodySprite
-  } from  "/src/index.js"
-  export function triangle(manager) {
-    let world = manager.getSystem("world")
-    
-    let box = Entity.Default(200,100)
-    let body = new Trigon(50,50 * Math.sin(30),60)
-    
-    box.attach("body",body)
-    .attach("sprite",new BodySprite())
-    manager.add(box)
-    world.gravity = 980
-  }
+  Entity,
+  Trigon,
+  BodySprite
+} from  "/src/index.js"
+export function triangle(manager) {
+  let world = manager.getSystem("world")
+  let angle = Math.PI/3
+  let box = Entity.Default(200,100,1)
+  let body = new Trigon(50,50 * Math.sin(angle),angle)
+  
+  box.attach("body",body)
+  .attach("sprite",new BodySprite())
+  manager.add(box)
+  world.gravity = 300
+}
