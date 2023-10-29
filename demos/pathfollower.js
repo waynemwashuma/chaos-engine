9bcbea8e78457a045e3da9c059f069af6fb9f0a9
94756970+waynemwashuma@users.noreply.github.com <94756970+waynemwashuma@users.noreply.github.com>
2023-10-29 12:54:33 +0300

    (demos)Updated parhfollowing demo #update

--- demos/pathfollower.js
+++ demos/pathfollower.js
@@ -14,28 +14,21 @@ export function pathfollower(manager) {
   manager.getSystem("world").gravity = 0
 
   let a = Entity.Default(100, 100)
-  let b = Entity.Default(100, 250)
 
   let aa = new Agent()
-  let ab = new Agent()
 
   let p = new Path()
-  p.tolerance = 60
+  p.tolerance = 10
 
   a.attach("body", new Ball(10))
-  b.attach("body", new Ball(20))
 
   a.attach("sprite", new AgentSprite())
-  b.attach("sprite", new BodySprite())
 
   a.attach("agent", aa)
-  b.attach("agent", ab)
   
   aa.add(new PathFollowing(p))
-  ab.add(new WanderBehaviour())
 
   manager.add(a)
-  manager.add(b)
 
   p.add(new Vector(100,50))
   p.add(new Vector(300,100))
