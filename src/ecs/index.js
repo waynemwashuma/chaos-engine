c4a2f4d3437848520b8608caf9878fc330ba4d3b
94756970+waynemwashuma@users.noreply.github.com <94756970+waynemwashuma@users.noreply.github.com>
2023-10-30 15:27:04 +0300

    [ecs] Created a boilerplate function for generating a manager with default systems. #new #ecs

--- src/ecs/index.js
+++ src/ecs/index.js
@@ -2,6 +2,10 @@ export * from "./manager.js"
 export * from "./component.js"
 export * from "./system.js"
 export * from "./entity.js"
+
 export * from "./transformComponent.js"
 export * from "./movableComponent.js"
 export * from "./boundsComponent.js"
+
+export * from "./createEntity.js"
+export * from "./createManager.js"
