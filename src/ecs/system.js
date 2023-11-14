cd1e891da0b9b583887c644bd0d62b5cff71f246
94756970+waynemwashuma@users.noreply.github.com <94756970+waynemwashuma@users.noreply.github.com>
2023-11-14 19:15:25 +0300

    [ecs] Refactored `System()` #refactor #internal

--- src/ecs/system.js
+++ src/ecs/system.js
@@ -1,36 +1,51 @@
-import { Utils } from "../utils/index.js"
-
+import { mixin, Err, Utils } from "../utils/index.js"
 
 /**
  * Updates components assigned to it.
  * 
  * @interface
-*/
-export class System{}
+ */
+export class System {
+  init() {
+    Err.warnOnce("Please override the init method in the system " + proto.constructor.name)
+  }
+  update() {
+    Err.warnOnce("Please override the update method in the system " + proto.constructor.name)
 
-Utils.inheritSystem(System)
+  }
+  add(component) {
+    this.objects.push(component)
+  }
+  remove(component) {
+    let index = this.objects.indexOf(component)
+    Utils.removeElement(this.objects, index)
+  }
+  static implement(system) {
+    mixin(System,system)
+  }
+}
 
 /**
  * 
  * @function
  * @name System#add
  * @param {Component} component
-*/
+ */
 /**
  * 
  * @function
  * @name System#remove
  * @param {Component} component
-*/
+ */
 /**
  * 
  * @function
  * @name System#init
  * @param {Manager} manager
  */
- /**
-  * 
-  * @function
-  * @name System#update
-  * @param {number} dt
-  */
+/**
+ * 
+ * @function
+ * @name System#update
+ * @param {number} dt
+ */
