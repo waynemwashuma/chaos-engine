faa259e615d1b18d71a3c6a0a3203a041872e6b8
94756970+waynemwashuma@users.noreply.github.com <94756970+waynemwashuma@users.noreply.github.com>
2023-11-15 13:39:15 +0300

    [ecs] `Component.implement()` to replace `Utils.inheritComponent()` #change

--- src/ecs/component.js
+++ src/ecs/component.js
@@ -1,25 +1,19 @@
-import { Utils } from "../utils/index.js"
+import { Utils, Err, mixin } from "../utils/index.js"
 
 
 /**
  * A helper class.
  * Since there are no interfaces in JavaScript,
  * you might have to extend this to create a component, but there is another solution.
- * Use instead Utils.inheritComponent() if you have your own hierarchy of classes.
+ * Use instead `Component.implement()` if you have your own hierarchy of classes.
  * In typescript,this would be an interface.
  * 
  * @interface
  * 
  */
 class Component {
-  /**
-   * @type Entity | null
-   */
-  entity = null
 
-  destroy() {
-    this.entity = null
-  }
+  destroy() {}
   /**
    * @type string
    */
@@ -33,25 +27,20 @@ class Component {
     return "component"
   }
   /**
-
    * @param {Entity} entity
-
   */
-  init(entity) {
-    this.entity = entity
-  }
+  init(entity) {}
   /**
    * @param {number} dt
    */
   update(dt) {
     Err.warnOnce("Please override the update function in the component " + proto.constructor.name)
-
   }
   /**
    * @param {string} n
    */
-  get(n) {
-    return this.entity.getComponent(n);
+  get(entity,n) {
+    return entity.getComponent(n);
   }
   /**
    * @param {...string} names
@@ -65,8 +54,8 @@ class Component {
    * @param {CircleBounding | BoxBounding} bound
    * @param {Entity} [target=[]]
    */
-  query(bound, target = []) {
-    return this.entity.query(bound, target)
+  query(entity,bound, target = []) {
+    return entity.query(bound, target)
   }
   static fromJson() {
     throw "Implement static method fromJson() in your component " + this.CHOAS_CLASSNAME
@@ -74,36 +63,10 @@ class Component {
   static toJson() {
     throw "Implement static method toJson() in your component " + this.CHOAS_CLASSNAME
   }
+  static implement(component) {
+    Utils.inheritComponent(component)
+  }
 }
-Utils.inheritComponent(Component)
 export {
   Component
 }
-/**
- * Destroys the component.
- * 
- * @function
- * @name Component#destroy
- */
-/**
- * Initializes a component.
- * 
- * @function
- * @name Component#init
- * @param {Entity} entity
- */
-/**
- * Updates a component.Called by the system which manages its type.
- * 
- * @function
- * @name Component#update
- * @param {number} dt
- */
-/**
- * Gets a component in the entity containing this entity.
- * 
- * @function
- * @name Component#requires
- * @param {string} ...names
- * @throws Qhen a named component isnt in the parent entity
- */
