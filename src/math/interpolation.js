2a319b37e454545aa3ba5fc90e70eb512ea52bd1
94756970+waynemwashuma@users.noreply.github.com <94756970+waynemwashuma@users.noreply.github.com>
2023-11-24 14:57:54 +0300

    [Math] Refactored Easing functions #refactor #change

--- src/math/interpolation.js
+++ src/math/interpolation.js
@@ -1,209 +1,162 @@
 export const Easing = {
-  Linear: {
-    In: function(x) {
-      return x;
-    },
-    Out: function(x) {
-      return x;
-    },
-    InOut: function(x) {
-      return x;
-    },
+  linear: function(x) {
+    return x;
   },
-  Quadratic: {
-    In: function(x) {
-      return x * x;
-    },
-    Out: function(x) {
-      return x * (2 - x);
-    },
-    InOut: function(x) {
-      if ((x *= 2) < 1) {
-        return 0.5 * x * x;
-      }
-      return -0.5 * (--x * (x - 2) - 1);
-    },
+  quadraticIn: function(x) {
+    return x * x;
   },
-  Cubic: {
-    In: function(x) {
-      return x * x * x;
-    },
-    Out: function(x) {
-      return --x * x * x + 1;
-    },
-    InOut: function(x) {
-      if ((x *= 2) < 1) {
-        return 0.5 * x * x * x;
-      }
-      return 0.5 * ((x -= 2) * x * x + 2);
-    },
+  quadraticOut: function(x) {
+    return x * (2 - x);
   },
-  Quartic: {
-    In: function(x) {
-      return x * x * x * x;
-    },
-    Out: function(x) {
-      return 1 - --x * x * x * x;
-    },
-    InOut: function(x) {
-      if ((x *= 2) < 1) {
-        return 0.5 * x * x * x * x;
-      }
-      return -0.5 * ((x -= 2) * x * x * x - 2);
-    },
+  quadraticInOut: function(x) {
+    if ((x *= 2) < 1) {
+      return 0.5 * x * x;
+    }
+    return -0.5 * (--x * (x - 2) - 1);
   },
-  Quintic: {
-    In: function(x) {
-      return x * x * x * x * x;
-    },
-    Out: function(x) {
-      return --x * x * x * x * x + 1;
-    },
-    InOut: function(x) {
-      if ((x *= 2) < 1) {
-        return 0.5 * x * x * x * x * x;
-      }
-      return 0.5 * ((x -= 2) * x * x * x * x + 2);
-    },
+  cubicIn: function(x) {
+    return x * x * x;
   },
-  Sinusoidal: {
-    In: function(x) {
-      return 1 - Math.sin(((1.0 - x) * Math.PI) / 2);
-    },
-    Out: function(x) {
-      return Math.sin((x * Math.PI) / 2);
-    },
-    InOut: function(x) {
-      return 0.5 * (1 - Math.sin(Math.PI * (0.5 - x)));
-    },
+  cubicOut: function(x) {
+    return --x * x * x + 1;
   },
-  Exponential: {
-    In: function(x) {
-      return x === 0 ? 0 : Math.pow(1024, x - 1);
-    },
-    Out: function(x) {
-      return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
-    },
-    InOut: function(x) {
-      if (x === 0) {
-        return 0;
-      }
-      if (x === 1) {
-        return 1;
-      }
-      if ((x *= 2) < 1) {
-        return 0.5 * Math.pow(1024, x - 1);
-      }
-      return 0.5 * (-Math.pow(2, -10 * (x - 1)) + 2);
-    },
+  cubicInOut: function(x) {
+    if ((x *= 2) < 1) {
+      return 0.5 * x * x * x;
+    }
+    return 0.5 * ((x -= 2) * x * x + 2);
   },
-  Circular: {
-    In: function(x) {
-      return 1 - Math.sqrt(1 - x * x);
-    },
-    Out: function(x) {
-      return Math.sqrt(1 - --x * x);
-    },
-    InOut: function(x) {
-      if ((x *= 2) < 1) {
-        return -0.5 * (Math.sqrt(1 - x * x) - 1);
-      }
-      return 0.5 * (Math.sqrt(1 - (x -= 2) * x) + 1);
-    },
+  quarticIn: function(x) {
+    return x * x * x * x;
   },
-  Elastic: {
-    In: function(x) {
-      if (x === 0) {
-        return 0;
-      }
-      if (x === 1) {
-        return 1;
-      }
-      return -Math.pow(2, 10 * (x - 1)) * Math.sin((x - 1.1) * 5 * Math.PI);
-    },
-    Out: function(x) {
-      if (x === 0) {
-        return 0;
-      }
-      if (x === 1) {
-        return 1;
-      }
-      return Math.pow(2, -10 * x) * Math.sin((x - 0.1) * 5 * Math.PI) + 1;
-    },
-    InOut: function(x) {
-      if (x === 0) {
-        return 0;
-      }
-      if (x === 1) {
-        return 1;
-      }
-      x *= 2;
-      if (x < 1) {
-        return -0.5 * Math.pow(2, 10 * (x - 1)) * Math.sin((x - 1.1) * 5 * Math.PI);
-      }
-      return 0.5 * Math.pow(2, -10 * (x - 1)) * Math.sin((x - 1.1) * 5 * Math.PI) + 1;
-    },
+  quarticOut: function(x) {
+    return 1 - --x * x * x * x;
   },
-  Back: {
-    In: function(x) {
-      var s = 1.70158;
-      return x === 1 ? 1 : x * x * ((s + 1) * x - s);
-    },
-    Out: function(x) {
-      var s = 1.70158;
-      return x === 0 ? 0 : --x * x * ((s + 1) * x + s) + 1;
-    },
-    InOut: function(x) {
-      var s = 1.70158 * 1.525;
-      if ((x *= 2) < 1) {
-        return 0.5 * (x * x * ((s + 1) * x - s));
-      }
-      return 0.5 * ((x -= 2) * x * ((s + 1) * x + s) + 2);
-    },
+  quarticInOut: function(x) {
+    if ((x *= 2) < 1) {
+      return 0.5 * x * x * x * x;
+    }
+    return -0.5 * ((x -= 2) * x * x * x - 2);
   },
-  Bounce: {
-    In: function(x) {
-      return 1 - Easing.Bounce.Out(1 - x);
-    },
-    Out: function(x) {
-      if (x < 1 / 2.75) {
-        return 7.5625 * x * x;
-      }
-      else if (x < 2 / 2.75) {
-        return 7.5625 * (x -= 1.5 / 2.75) * x + 0.75;
-      }
-      else if (x < 2.5 / 2.75) {
-        return 7.5625 * (x -= 2.25 / 2.75) * x + 0.9375;
-      }
-      else {
-        return 7.5625 * (x -= 2.625 / 2.75) * x + 0.984375;
-      }
-    },
-    InOut: function(x) {
-      if (x < 0.5) {
-        return Easing.Bounce.In(x * 2) * 0.5;
-      }
-      return Easing.Bounce.Out(x * 2 - 1) * 0.5 + 0.5;
-    },
+  quinticIn: function(x) {
+    return x * x * x * x * x;
   },
-  generatePow: function(power) {
-    if (power === void 0) { power = 4; }
-    power = power < Number.EPSILON ? Number.EPSILON : power;
-    power = power > 10000 ? 10000 : power;
-    return {
-      In: function(x) {
-        return Math.pow(x, power);
-      },
-      Out: function(x) {
-        return 1 - Math.pow((1 - x), power);
-      },
-      InOut: function(x) {
-        if (x < 0.5) {
-          return Math.pow((x * 2), power) / 2;
-        }
-        return (1 - Math.pow((2 - x * 2), power)) / 2 + 0.5;
-      },
-    };
+  quinticOut: function(x) {
+    return --x * x * x * x * x + 1;
+  },
+  quinticInOut: function(x) {
+    if ((x *= 2) < 1) {
+      return 0.5 * x * x * x * x * x;
+    }
+    return 0.5 * ((x -= 2) * x * x * x * x + 2);
+  },
+  sinusoidalIn: function(x) {
+    return 1 - Math.sin(((1.0 - x) * Math.PI) / 2);
+  },
+  sinusoidalOut: function(x) {
+    return Math.sin((x * Math.PI) / 2);
+  },
+  sinusoidalInOut: function(x) {
+    return 0.5 * (1 - Math.sin(Math.PI * (0.5 - x)));
+  },
+  exponentialIn: function(x) {
+    return x === 0 ? 0 : Math.pow(1024, x - 1);
+  },
+  exponentialOut: function(x) {
+    return x === 1 ? 1 : 1 - Math.pow(2, -10 * x);
+  },
+  exponentialInOut: function(x) {
+    if (x === 0) {
+      return 0;
+    }
+    if (x === 1) {
+      return 1;
+    }
+    if ((x *= 2) < 1) {
+      return 0.5 * Math.pow(1024, x - 1);
+    }
+    return 0.5 * (-Math.pow(2, -10 * (x - 1)) + 2);
+  },
+  circularIn: function(x) {
+    return 1 - Math.sqrt(1 - x * x);
+  },
+  circularOut: function(x) {
+    return Math.sqrt(1 - --x * x);
+  },
+  circularInOut: function(x) {
+    if ((x *= 2) < 1) {
+      return -0.5 * (Math.sqrt(1 - x * x) - 1);
+    }
+    return 0.5 * (Math.sqrt(1 - (x -= 2) * x) + 1);
+  },
+  elasticIn: function(x) {
+    if (x === 0) {
+      return 0;
+    }
+    if (x === 1) {
+      return 1;
+    }
+    return -Math.pow(2, 10 * (x - 1)) * Math.sin((x - 1.1) * 5 * Math.PI);
+  },
+  elasticOut: function(x) {
+    if (x === 0) {
+      return 0;
+    }
+    if (x === 1) {
+      return 1;
+    }
+    return Math.pow(2, -10 * x) * Math.sin((x - 0.1) * 5 * Math.PI) + 1;
+  },
+  elasticInOut: function(x) {
+    if (x === 0) {
+      return 0;
+    }
+    if (x === 1) {
+      return 1;
+    }
+    x *= 2;
+    if (x < 1) {
+      return -0.5 * Math.pow(2, 10 * (x - 1)) * Math.sin((x - 1.1) * 5 * Math.PI);
+    }
+    return 0.5 * Math.pow(2, -10 * (x - 1)) * Math.sin((x - 1.1) * 5 * Math.PI) + 1;
+  },
+  backIn: function(x) {
+    var s = 1.70158;
+    return x === 1 ? 1 : x * x * ((s + 1) * x - s);
+  },
+  backOut: function(x) {
+    var s = 1.70158;
+    return x === 0 ? 0 : --x * x * ((s + 1) * x + s) + 1;
+  },
+  backInOut: function(x) {
+    var s = 1.70158 * 1.525;
+    if ((x *= 2) < 1) {
+      return 0.5 * (x * x * ((s + 1) * x - s));
+    }
+    return 0.5 * ((x -= 2) * x * ((s + 1) * x + s) + 2);
+  },
+  bounceIn: function(x) {
+    return 1 - Easing.bounceOut(1 - x);
+  },
+  bounceOut: function(x) {
+    if (x < 1 / 2.75) {
+      return 7.5625 * x * x;
+    }
+    else if (x < 2 / 2.75) {
+      return 7.5625 * (x -= 1.5 / 2.75) * x + 0.75;
+    }
+    else if (x < 2.5 / 2.75) {
+      return 7.5625 * (x -= 2.25 / 2.75) * x + 0.9375;
+    }
+    else {
+      return 7.5625 * (x -= 2.625 / 2.75) * x + 0.984375;
+    }
+  },
+  bounceInOut: function(x) {
+    if (x < 0.5) {
+      return Easing.bounceIn(x * 2) * 0.5;
+    }
+    return Easing.bounceOut(x * 2 - 1) * 0.5 + 0.5;
   },
 }
 
