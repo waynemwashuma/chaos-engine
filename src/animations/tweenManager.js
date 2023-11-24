1bacd6e93c559c2c5d5c4bb200f9614b3333ed97
94756970+waynemwashuma@users.noreply.github.com <94756970+waynemwashuma@users.noreply.github.com>
2023-11-24 15:00:35 +0300

    [Animations] New class `Tween()` for animating objects #new

--- src/animations/tweenManager.js
+++ src/animations/tweenManager.js
@@ -2,13 +2,13 @@ import {System} from "../ecs/index.js"
 
 export class TweenManager extends System {
   objects = []
-  init() {
-    manager.setComponentList("sprite", this.objects)
+  init(manager) {
+    manager.setComponentList("tween", this.objects)
   }
 
   update(dt) {
-    for (var i = 0; i < this._objects.length; i++) {
-      let tween = this._objects[i]
+    for (var i = 0; i < this.objects.length; i++) {
+      let tween = this.objects[i]
 
       tween.update(dt)
     }
