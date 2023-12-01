f8d87f8113dd69aa82fdb2df369d65d114d89512
94756970+waynemwashuma@users.noreply.github.com <94756970+waynemwashuma@users.noreply.github.com>
2023-12-02 01:44:08 +0300

    [Utils] Refactored code #refactor

--- src/utils/error.js
+++ src/utils/error.js
@@ -2,56 +2,46 @@ let marker = `🚀Chaos Engine Says::::\n`
 let mess = []
 
 /**
- * A set of functions to streamline logging of items to the console
- */
-export const Err = {}
-
-/**
  * Logs out a warning to the console.
- * 
- * @memberof Err
+ *
  * @param {string} message
  */
-Err.warn = function(message) {
+export function warn(message) {
   console.warn(marker + message)
 }
 
 /**
  * Throws a fatal error.
  * 
- * @memberof Err
  * @param {string} message
  */
-Err.throw = function(message) {
+export function throws(message) {
   throw new Error(marker + message)
 }
 
 /**
  * Logs out a non fatal error to the console.
  * 
- * @memberof Err
  * @param {string} message
  */
-Err.error = function(message) {
+export function error(message) {
   console.error(marker + message)
 }
 
 /**
  * Logs out a message to the console.
  * 
- * @memberof Err
  * @param {string} message
  */
-Err.log = function(message) {
+export function log(message) {
   console.log(marker + message);
 }
 /**
  * Logs out a warning once to the console.
  * 
- * @memberof Err
  * @param {string} message
  */
-Err.warnOnce = function warnOnce(message) {
+export function warnOnce(message) {
   if (mess.includes(message)) return
   mess.push(message)
   Err.warn(message)
@@ -59,12 +49,11 @@ Err.warnOnce = function warnOnce(message) {
 /**
  * Logs out a message,warning or error to the console according to the supplied log function.
  * 
- * @memberof Err
  * @param {boolean} test
  * @param {string} message
  * @param {Function} errfunc
  */
-Err.assert = function(test, errfunc, message) {
+export function assert(test, errfunc, message) {
   if (!test) errfunc(message)
   return test
 }
@@ -72,10 +61,9 @@ Err.assert = function(test, errfunc, message) {
 /**
  * Logs out a warning to the console.
  * 
- * @memberof Err
  * @param {string} message
  */
-Err.deprecate = function deprecate(original, replacement = "") {
+export function deprecate(original, replacement = "") {
   let message = `"${original}" has been depreciated.`
   if (replacement !== "")
     message += `Use "${replacement}" instead.`
