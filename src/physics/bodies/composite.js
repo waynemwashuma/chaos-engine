61c5cfe3ba59cc482036a5daeb4c13553972ee2e
94756970+waynemwashuma@users.noreply.github.com <94756970+waynemwashuma@users.noreply.github.com>
2023-12-06 18:36:03 +0300

    [Physics] Fixed bug on `Composite()` where it doesn't move when added to `World()` #fix

--- src/physics/bodies/composite.js
+++ src/physics/bodies/composite.js
@@ -5,7 +5,7 @@ import { ObjType } from "../settings.js"
 /**
  * Holds a group of related bodies and constraints.
  */
-export class Composite {
+export class Composite extends Component {
   /**
    * Entity this belongs to.
    * 
@@ -43,7 +43,7 @@ export class Composite {
     this.bodies.forEach(e => {
       e.init(entity, true)
     })
-    this.requires(entity,"transform")
+    this.requires(entity, "transform")
 
   }
   /**
@@ -130,13 +130,13 @@ export class Composite {
     }
     return mass
   }
-  set type(x){
+  set type(x) {
     for (var i = 0; i < this.bodies.length; i++) {
       this.bodies[i].type = x
     }
   }
-  get type(){
-    return this.bodies[0]?.type 
+  get type() {
+    return this.bodies[0]?.type
   }
   /**
    * Density of a body.
@@ -208,5 +208,9 @@ export class Composite {
       this.bodies[i].angularVelocity = x
     }
   }
+  destroy() {
+    this.bodies.forEach(b => {
+      b.destroy()
+    })
+  }
 }
-Component.implement(Composite)
