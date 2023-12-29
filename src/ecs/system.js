5bd90f5e320a7e5dfc0d46ae1f46207263d7c00f
94756970+waynemwashuma@users.noreply.github.com <94756970+waynemwashuma@users.noreply.github.com>
2023-12-29 12:23:14 +0300

    [Documentation] `System()` documented properly #change

--- src/ecs/system.js
+++ src/ecs/system.js
@@ -2,50 +2,38 @@ import { mixin, Err, Utils } from "../utils/index.js"
 
 /**
  * Updates components assigned to it.
- * 
- * @interface
  */
 export class System {
-  init() {
+  /**
+   * @param {Manager} manager
+   */
+  init(manager) {
     Err.warnOnce("Please override the init method in the system " + this.constructor.name)
   }
-  update() {
+  /**
+   * @param {number} dt
+   */
+  update(dt) {
     Err.warnOnce("Please override the update method in the system " + this.constructor.name)
-
   }
+  /**
+   * @param {Component} component
+   */
   add(component) {
     this.objects.push(component)
   }
+  /**
+   * @param {Component} component
+   */
   remove(component) {
     let index = this.objects.indexOf(component)
     Utils.removeElement(this.objects, index)
   }
+  //Todo - Fix this
+  /**
+   * @param {any} system
+   */
   static implement(system) {
-    mixin(System,system)
+    mixin(System, system)
   }
 }
-
-/**
- * 
- * @function
- * @name System#add
- * @param {Component} component
- */
-/**
- * 
- * @function
- * @name System#remove
- * @param {Component} component
- */
-/**
- * 
- * @function
- * @name System#init
- * @param {Manager} manager
- */
-/**
- * 
- * @function
- * @name System#update
- * @param {number} dt
- */
