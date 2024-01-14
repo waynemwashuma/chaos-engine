8d4c985355095b59d4a8f658fc2590a7462d2e62
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-01-14 07:33:42 +0300

    [scripts] Fixing up typedef script #fix

--- scripts/typedef.js
+++ scripts/typedef.js
@@ -1,20 +1,20 @@
-const fs = require("fs")
-const path = require("path")
+import { readFileSync ,writeFileSync,readdirSync } from "fs";
+import { join,resolve } from "path";
 
-let inputDir1 = path.resolve(process.cwd(),"src","typedef")
-let inputDir2 = path.resolve(process.cwd(),"dist")
-let files = fs.readdirSync(inputDir1)
-let outputfiles = fs.readdirSync(inputDir2)
+let inputDir1 = resolve(process.cwd(),"src","typedef")
+let inputDir2 = resolve(process.cwd(),"dist")
+let files = readdirSync(inputDir1)
+let outputfiles = readdirSync(inputDir2)
 
 let blob = ""
 for (const file of files) {
-    blob += fs.readFileSync(path.join(inputDir1,file),{encoding:"utf-8"})
+    blob += readFileSync(join(inputDir1,file),{encoding:"utf-8"})
 }
 
 for (const filename of outputfiles) {
     if(filename.includes('.d.ts') || filename.includes('.min.'))continue
 
-    let output = fs.readFileSync(path.join(inputDir2,filename),{encoding:"utf-8"}) + blob
+    let output = readFileSync(join(inputDir2,filename),{encoding:"utf-8"}) + blob
 
-    fs.writeFileSync(path.join(inputDir2,filename),output)
+    writeFileSync(join(inputDir2,filename),output)
 }
