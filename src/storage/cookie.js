ae5443900e54a2ce3c3a51d15b61444acdf96e19
94756970+waynemwashuma@users.noreply.github.com <94756970+waynemwashuma@users.noreply.github.com>
2024-01-15 21:48:21 +0300

    [Storage] Fixed `Cookie.get()` #fix

--- src/storage/cookie.js
+++ src/storage/cookie.js
@@ -2,7 +2,7 @@
  * Used to manipulate and read from the cookie string.
  * 
  * @module Cookie
-*/
+ */
 
 export const Cookies = {
   /**
@@ -11,8 +11,8 @@ export const Cookies = {
    * @param {string} n Key of the cookie.
    * @param {string} v The value of the cookie.
    * @param {number} [=12000] Max age of the cookie before it is deleted.
-  */
-  set(n, v, maxAge= 12000) {
+   */
+  set(n, v, maxAge = 12000) {
     document.cookie = `${n}=${v};maxAge=${maxAge}`
   },
   /**
@@ -20,26 +20,25 @@ export const Cookies = {
    * 
    * @param {string} n Key of the cookie
    * @returns {string}
-  */
+   */
   get(n) {
     let arr = document.cookie.split(";")
     for (var i = 0; i < arr.length; i++) {
       let pair = arr[i].split('=')
-      if (pair[0].includes(n))
-        return pair[1]
+      if (pair[0] === n) return pair[1]
     }
   },
   /**
    * Removes a cookie by its key from cookies.
    * 
    * @param {string} n Key of the cookie
-  */
-  delete(n){
+   */
+  delete(n) {
     document.cookie = `${n}=; max-age=0`;
   },
   /**
    * Removes all cookies that are contained on the document.
-  */
+   */
   clear() {
     let arr = document.cookie.split(";")
     for (var i = 0; i < arr.length; i++) {
