68e4a69f4f3c17d72b370e2e4f608de9392d2aa8
94756970+waynemwashuma@users.noreply.github.com <94756970+waynemwashuma@users.noreply.github.com>
2024-01-15 21:25:42 +0300

    [loader] Added a new loader `TextureLoader()` to load in images as textures #new

---
+++ src/loader/textureloader.js
@@ -0,0 +1,53 @@
+import { DEVICE } from "../device/index.js"
+import { Err } from "../utils/index.js"
+import { getURLName, getURLExtension } from "./utils.js"
+
+export class TextureLoader {
+  resources = []
+  onSingleFinish = () => {}
+  onfinish = () => {}
+  baseUrl = ""
+  async load(urls) {
+    for (var url of urls) {
+      const name = getURLName(url)
+      const extension = getURLExtension(url)
+
+      if (!DEVICE.supportedImages.includes(extension)) {
+        Err.error("`ImageLoader()` could not load in \"" + url + "\" : Unsupported extension name.")
+        continue
+      }
+
+      const request = await fetch(url)
+
+      if (!request.ok) {
+        Err.error("`ImageLoader()` could not load in \"" + url + "\" : Resource not found.")
+        continue
+      }
+
+      const raw = await request.arrayBuffer()
+      const imgUrl = URL.createObjectURL(new Blob([raw]))
+      const dimensions = await getDimensions(imgUrl)
+
+      URL.revokeObjectURL(imgUrl)
+      this.resources[name] = {
+        buffer: raw,
+        dimensions
+      }
+      this.onSingleFinish()
+    }
+    this.onfinish()
+  }
+}
+
+function getDimensions(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image()
+    img.onload = e => {
+      resolve({
+        width: img.width,
+        height: img.height
+      })
+    }
+    img.src = url
+  })
+}
