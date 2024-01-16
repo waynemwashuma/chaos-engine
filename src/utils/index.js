54b9736751d60340d9c02dfd7c750de83b4d1c93
94756970+waynemwashuma@users.noreply.github.com <94756970+waynemwashuma@users.noreply.github.com>
2024-01-16 15:58:37 +0300

    [Utils] Removed `Err` as it is replaced by `Logger` #removed

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,12 +5,5 @@
  */
 export * as Utils from "./common.js"
 export {mixin} from "./common.js"
-
-/**
- * A set of functions to streamline logging of items to the console
- * 
- * @module
- */
-export * as Err from "./error.js"
 export * from "./clock.js"
 export * from "./perf.js"
