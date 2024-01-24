ae293079eaf09f8e54229a858320412b4a951003
94756970+waynemwashuma@users.noreply.github.com <94756970+waynemwashuma@users.noreply.github.com>
2024-01-25 00:55:10 +0300

    [math] Added a new method `Ovwrlaps.colliding` to check if two `boundingCircle()` and/or `BoundingSquare()` are colliding #new

--- src/math/AABB/overlap.js
+++ src/math/AABB/overlap.js
@@ -27,12 +27,12 @@ export const Overlaps = {
       (a.pos.y - b.pos.y) * (a.pos.y - b.pos.y)
     return distance < a.r * a.r + b.r * b.r;
   },
-    /**
-     * Checks if An AABB and a CircleBound overlap
-     * 
-     * @param {BoundingBox} a
-     * @param {BoundingCircle} b
-     */
+  /**
+   * Checks if An AABB and a CircleBound overlap
+   * 
+   * @param {BoundingBox} a
+   * @param {BoundingCircle} b
+   */
   AABBvsSphere(a, b) {
     const x = Math.max(a.min.x, Math.min(b.pos.x, a.max.x));
     const y = Math.max(a.min.y, Math.min(b.pos.y, a.max.y));
@@ -41,5 +41,20 @@ export const Overlaps = {
       (y - b.pos.y) * (y - b.pos.y)
 
     return distance < b.r * b.r;
+  },
+  /**
+   * Checks if any AABB or/and a BoundingCircle overlap
+   * 
+   * @param {BoundingBox | BoundingCircle} a
+   * @param {BoundingCircle | BoundingBox} b
+   */
+  colliding(bound1, bound2) {
+    if (bound1.max && bound2.max)
+      return Overlaps.AABBColliding(bound1, bound2)
+    if (bound1.r && bound2.r)
+      return Overlaps.boundSpheresColliding(bound1, bound2)
+    if (bound1.r)
+      return Overlaps.AABBvsSphere(bound2, bound1)
+    return Overlaps.AABBvsSphere(bound1, bound2)
   }
 }
