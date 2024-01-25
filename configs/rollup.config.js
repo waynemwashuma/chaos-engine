a6c9d7278a0044366ef0dc6901c8fac5a8030ea6
94756970+waynemwashuma@users.noreply.github.com <94756970+waynemwashuma@users.noreply.github.com>
2024-01-25 05:01:47 +0300

    [config] Updated the rollup banner

--- configs/rollup.config.js
+++ configs/rollup.config.js
@@ -3,16 +3,21 @@ pkg.name = "chaos"
 const fs = require("fs")
 const input = "src/index.js";
 const created = `2023-${new Date().getFullYear()}`
+
 const license = fs.readFileSync('LICENSE', 'utf8');
 const banner = `/*
  * @author ${pkg.author}
  * {@link ${pkg.repository.url}}
  * @copyright  ${created} ${pkg.author}
- * @license ${pkg.license}
  *
- * 
- * ${license}
- */`
+ * @license ${pkg.license}
+ * @version ${pkg.version}
+ */
+ /*
+ ${license}
+ */
+ `
+ 
 module.exports = [{
     // UMD
     input,
