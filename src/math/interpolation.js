aeaa46d6c9beea3ae9c8a4fdb402776808641e25
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-01-30 05:05:49 +0300

    [math] Added new method `Interpolation.cosine()` for cosine interpolation

--- src/math/interpolation.js
+++ src/math/interpolation.js
@@ -305,4 +305,8 @@ export const Interpolation = {
 
     return (2 * p1 - 2 * p2 + v0 + v1) * t3 + (-3 * p1 + 3 * p2 - 2 * v0 - v1) * t2 + v0 * t + p1
   },
+  cosine(p0, p1, t) {
+    const c = (1 - Math.cos(t * 3.1415927)) * .5
+    return (1 - c) * p0 + c * p1
+  }
 }
