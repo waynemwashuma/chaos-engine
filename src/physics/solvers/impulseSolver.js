94c3476bec28ae823abf1495609d8efc720beb0a
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-03-06 13:33:01 +0300

    [Physics] Refactored Collision manifold generation in narrowphase. #refactor

--- src/physics/solvers/impulseSolver.js
+++ src/physics/solvers/impulseSolver.js
@@ -8,61 +8,28 @@ let tmp1 = new Vector2(),
  * Solves for the collision normal impulse of a given body pair.
  */
 export class ImpulseSolver {
-
-  static solve(movableA, movableB, bodyA, bodyB, manifold) {
-    const { ca1, ca2, restitution } = manifold
-    const { axis } = manifold.contactData
-    let a$va = tmp1.set(ca1.y * -movableA.rotation, ca1.x * movableA.rotation)
-    let a$vb = tmp2.set(ca2.y * -movableB.rotation, ca2.x * movableB.rotation)
-    let va = tmp3.copy(movableA.velocity).add(a$va)
-    let vb = tmp4.copy(movableB.velocity).add(a$vb)
-    let vp = va.sub(vb)
-    let vp_p = axis.dot(vp);
-
-    if (vp_p >= 0) {
-      manifold.impulse = 0
-      return
-    }
-    ////this used to be -(1 + restitution) but when i changed it,stability ensued.
-    //i think the reason of the previous instability is because i changed the penetration solver to be impulse based from position based.
-    //so.. i came back to that....
-    let j = -(1 + restitution) * vp_p / (
-      (bodyA.inv_mass + bodyB.inv_mass) +
-      sq(ca1.cross(axis)) * bodyA.inv_inertia +
-      sq(ca2.cross(axis)) * bodyB.inv_inertia)
-    let jn = axis.clone().multiply(j)
-    let ang1 = ca1.cross(jn) * bodyA.inv_inertia;
-    let ang2 = ca2.cross(jn) * -bodyB.inv_inertia;
-    let vel1 = jn.clone().multiply(bodyA.inv_mass)
-    let vel2 = jn.clone().multiply(-bodyB.inv_mass)
-    manifold.velA.copy(vel1)
-    manifold.velB.copy(vel2)
-    manifold.rotA = ang1
-    manifold.rotB = ang2
-    manifold.impulse = j
-  }
   static solveWithFriction(movableA, movableB, bodyA, bodyB, manifold) {
-
+    manifold.impulse = 0
+    
     const sf = manifold.staticFriction
     const kf = manifold.kineticFriction
     const { ca1, ca2, restitution } = manifold
+    
     const { axis: normal } = manifold.contactData
-    const tangent = normal.normal(1)
-    tangent.multiply(tangent.dot(tmp1.copy(movableB.velocity).sub(movableA.velocity)))
-    tangent.normalize()
-    //console.log(tangent)
     const a$va = new Vector2().set(ca1.y * -movableA.rotation, ca1.x * movableA.rotation)
     const a$vb = new Vector2().set(ca2.y * -movableB.rotation, ca2.x * movableB.rotation)
     const va = a$va.add(movableA.velocity)
     const vb = a$vb.add(movableB.velocity)
     const relVel = va.sub(vb)
-    let vp_n = normal.dot(relVel);
+    
+    const tangent = normal.normal(1)
+    tangent.multiply(tangent.dot(relVel))
+    tangent.normalize()
+    
+    const vp_n = normal.dot(relVel);
     const vp_t = tangent.dot(relVel)
 
-    if (vp_n >= 0) {
-      manifold.impulse = 0
-      vp_n = 0
-    }
+    if (vp_n >= 0) return
 
     const j = -(1 + restitution) * vp_n / (
       (bodyA.inv_mass + bodyB.inv_mass) +
