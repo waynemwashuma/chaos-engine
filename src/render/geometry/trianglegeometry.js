1339797a6afe91d732de967c75bb4cb3a0c77112
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-03-11 15:10:46 +0300

    [Render] update trianglegeometry.js #change #internal

--- src/render/geometry/trianglegeometry.js
+++ src/render/geometry/trianglegeometry.js
@@ -1,22 +1,24 @@
-import {BufferGeometry} from "./geometry.js"
-import {Vector2} from "../../math/index.js"
+import { BufferGeometry } from "./geometry.js"
+import { Vector2 } from "../../math/index.js"
 
-const tmp1 = new Vector2()
-export class TriangleGeometry extends BufferGeometry{
+export class TriangleGeometry extends BufferGeometry {
   /**
    * @param {number} base
    * @param {number} height
    * @param {number} angle
    */
-  constructor(base, height, angle = Math.asin(height/base) ) {
-    let l1 = new Vector2(1).multiply(base)
-    let l2 = Vector2.fromAngle(angle).multiply(-height/Math.sin(angle))
-    let center = tmp1.set((l1.x + l2.x) / 3, l2.y / 3)
+  constructor(base, height, angle = Math.asin(height / base)) {
+    const l1 = new Vector2(1).multiply(base)
+    const l2 = Vector2.fromAngle(angle).multiply(-height / Math.sin(angle))
+    const center = new Vector2((
+        l1.x + l2.x) / 3,
+      l2.y / 3
+    )
     super()
     BufferGeometry.setAttribute(this, "position", [
-      new Vector2().sub(center),
-      l1.sub(center),
-      l2.sub(center)
+            l2.sub(center),
+            l1.sub(center),
+            center.reverse()
     ])
     BufferGeometry.initCanvas2D(this)
   }
