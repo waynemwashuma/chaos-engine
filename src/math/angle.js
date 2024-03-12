789cc5cb34a419696a8f2a5ab5fefa4c0ab50bde
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-03-12 21:03:31 +0300

    [Math] Refactored `Angle()` as a cleanup #refactor

--- src/math/angle.js
+++ src/math/angle.js
@@ -4,7 +4,7 @@ import { Logger } from "../logger/index.js"
  * Wrapper class since JavaScript doesn't support references to numbers explicitly.
  * Keeps record of the orientation of an entity.
  */
-class Angle {
+export class Angle {
 
   /**
    * Orientation in radians.
@@ -12,40 +12,12 @@ class Angle {
    * @private
    * @type {number}
    */
-  _value = 0
-  /**
-   * Cosine of the angle.
-   * 
-   * @private
-   * @type {number}
-   */
-  _cos = 0
-  /**
-   * Cosine of the angle.
-   * 
-   * @private
-   * @type {number}
-   */
-  _sin = 0
+  value = 0
   /**
    * @param {number} [rad=0] angle in radians.
    */
-
-  //TODO - Change this to radians instead
   constructor(rad = 0) {
-    this.value = rad * Math.PI/180
-  }
-  /**
-   * @type {string}
-   */
-  get CHOAS_CLASSNAME() {
-    return this.constructor.name.toLowerCase()
-  }
-  /**
-   * @type {string}
-   */
-  get CHAOS_OBJ_TYPE() {
-    return "angle"
+    this.value = rad
   }
   /**
    * The orientation in degrees.
@@ -55,85 +27,29 @@ class Angle {
     Logger.deprecate("Angle.degree")
     this.value = x * Math.PI / 180
   }
-  get _rad(){
-    return this._value
-  }
-  set _rad(x){
-    this._value = x
-  }
-  get _deg(){
-    return this.value * 180/Math.PI
-  }
-  set _deg(x){
-    this.value = x * Math.PI/180
-  }
   /**
    * The orientation in radians.
    * 
    * @deprecated
    */
   set radian(x) {
-    Logger.deprecate("Angle.radian", "Angle.value")
+    Logger.deprecate("Angle().radian", "Angle().value")
     this.value = x
   }
   get radian() {
-    Logger.deprecate("Angle.radian", "Angle.value")
+    Logger.deprecate("Angle().radian", "Angle().value")
     return this.value
   }
   get degree() {
-    Logger.deprecate("Angle.degree")
+    Logger.deprecate("Angle().degree")
     return this.value * 180/Math.PI
-  }
-  /**
-   * The angle in radians.
-   * @type number
-   */
-  get value() {
-    return this._value
-  }
-  set value(x) {
-    this._value = x
-    this._cos = Math.cos(x)
-    this._sin = Math.sin(x)
-  }
-  /**
-   * @type number
-   */
-  get cos() {
-    return this._cos
-  }
-  /**
-   * @type number
-   */
-  get sin() {
-    return this._sin
   }
   /**
    * Copies the orientation of another angle.
    * 
    * @param {Angle} angle
    */
-  copy(angle) {
+  static copy(angle) {
     this.value = angle.value
   }
-
-  /**
-   * @param {*} obj
-   */
-  fromJSON(obj) {
-    this.value = obj.val
-  }
-  /**
-   * 
-   */
-  toJson() {
-    return {
-      deg: this.value,
-      type: this.CHAOS_OBJ_TYPE
-    }
-  }
-}
-
-export {
-  Angle
 }
