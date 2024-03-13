aa7e5079610a5543fdaccbf398efdcdb89ac5126
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-03-13 19:04:38 +0300

    [Loader] Refactored `SoundLoader()` and `ImageLoader()` #refactor #internal

--- src/loader/textureloader.js
+++ src/loader/textureloader.js
@@ -1,55 +1,32 @@
 import { DEVICE } from "../device/index.js"
-import { Logger } from "../logger/index.js"
-import { getURLName, getURLExtension } from "./utils.js"
+import { Loader } from "./loader.js"
 
-export class TextureLoader {
-  /**
-   * @type {{ [x: string]: { buffer: ArrayBuffer, dimensions: any; } }}
-   */
-  resources = {}
-  onSingleFinish = () => {}
-  onfinish = () => {}
-  baseUrl = ""
+export class ImageLoader extends Loader {
+  verify(extension) {
+    if (DEVICE.supportedImages.includes(extension)) return true
+    return false
+  }
   /**
    * @param {string[]} urls
    */
-  async load(urls) {
-    for (const url of urls) {
-      const name = getURLName(url)
-      const extension = getURLExtension(url)
+  parse(request) {
+    if (!request.ok) return
 
-      if (!DEVICE.supportedImages.includes(extension)) {
-        Logger.error("`ImageLoader()` could not load in \"" + url + "\" : Unsupported extension name.")
-        continue
-      }
-
-      const request = await fetch(url)
-
-      if (!request.ok) {
-        Logger.error("`ImageLoader()` could not load in \"" + url + "\" : Resource not found.")
-        continue
-      }
-
-      const raw = await request.arrayBuffer()
-      const imgUrl = URL.createObjectURL(new Blob([raw]))
-      const dimensions = await getDimensions(imgUrl)
-
-      URL.revokeObjectURL(imgUrl)
-      this.resources[name] = {
-        buffer: raw,
-        dimensions
-      }
-      this.onSingleFinish()
+    const raw = await request.arrayBuffer()
+    const dimensions = await getDimensions(raw)
+    return {
+      buffer: raw,
+      dimensions
     }
-    this.onfinish()
   }
 }
 
 /**
  * @param {string} url
  */
-function getDimensions(url) {
+function getDimensions(raw) {
   return new Promise((resolve, reject) => {
+    const imgUrl = URL.createObjectURL(new Blob([raw]))
     const img = new Image()
     img.onload = e => {
       resolve({
@@ -58,5 +35,6 @@ function getDimensions(url) {
       })
     }
     img.src = url
+    URL.revokeObjectURL(imgUrl)
   })
 }
