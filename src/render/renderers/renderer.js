972bc32c064a9444dbff8cd1865f9e36ca414e35
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-03-14 19:32:37 +0300

    [render] Typed `Renderer` #internal

--- src/render/renderers/renderer.js
+++ src/render/renderers/renderer.js
@@ -19,7 +19,6 @@ export class Renderer {
    */
   camera
   /**
-   * @param {CanvasRenderingContext2D} context
    * @param {HTMLCanvasElement} canvas element to draw on
    */
   constructor(canvas) {
@@ -28,13 +27,13 @@ export class Renderer {
   }
   /**
    * Attaches the renderer to a given html element by its selector.
-   * 
    * @param {string} selector A css selector string that is passed to document.querySelector
-   * @param {true} focus whether to shift focus of input to the element pr not
+   * @param {boolean} focus whether to shift focus of input to the element pr not
+   * @param {Renderer} renderer
    */
-  static bindTo(renderer,selector, focus = true) {
+  static bindTo(renderer,selector,focus = true) {
     let element = document.querySelector(selector)
-    if(!element)return console.error("could not find container for the canvas.");
+    if (!element) return console.error("could not find container for the canvas.");
     renderer.domElement.remove()
     renderer.domElement.style.backgroundColor = "grey"
     renderer.domElement.style.touchAction = "none"
@@ -42,17 +41,21 @@ export class Renderer {
   }
   /**
    * Requests fullscreen for the renderer.
+   * 
+   * @param {Renderer} renderer
    */
   static requestFullScreen(renderer) {
-    renderer.domElement.parentElement.requestFullscreen()
+    const parent = renderer.domElement.parentElement
+    if (parent) return parent.requestFullscreen()
+    return renderer.domElement.requestFullscreen()
   }
   /**
    * Sets the width and height of the canvas being rendered to.
-   * 
    * @param {number} w Width of the canvas.
    * @param {number} h Height of the canvas.
+   * @param {Renderer} renderer
    */
-  static setViewport(renderer,w, h) {
+  static setViewport(renderer,w,h) {
     const canvas = renderer.domElement
     canvas.style.width = w + "px"
     canvas.style.height = h + "px"
