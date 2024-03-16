9b2dea36a512aadb5b48485803b09e7ba6415c87
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-03-16 21:44:22 +0300

    [Physics] Added new plugins related to physics including `Physics2DPlugin()` , `Broadphase2DPlugin()` ,`Narrowphase2DPlugin()` and `Intergrator2DPlugin()` #new

--- src/debuggers/bodydebugger.js
+++ src/debuggers/bodydebugger.js
@@ -9,15 +9,17 @@ import { Manager } from "../ecs/index.js"
  * @param {Renderer2D} renderer
  */
 // @ts-ignore
-export function bodyDebugger(manager, renderer, options = {}) {
+export function bodyDebugger(manager, options = {}) {
   options.clearRenderer = options.clearRenderer || false
   options.drawCollisionArm = options.drawCollisionArm || false
   options.drawContacts = options.drawContacts || false
   manager.registerSystem(dt => {
-    const [transform, movable, bounds, bodies] = manager.query("transform", "movable", "bounds", "body").raw()
+    const [transform, movable, bounds, bodies] = manager.query("transform", "movable", "bound", "body").raw()
     const clmd = manager.queryEvent("collision")
-
-    if (options.clearRenderer) Renderer2D.clear(renderer)
+    const viewport = manager.getResource("renderer")
+    const ctx = manager.getResource("ctx")
+    
+    if (options.clear) ctx.clearRect(0,0,viewPort.width,viewport.height)
     if (options.drawCollisionArm) drawArms()
     if (options.drawPosition) drawPositions()
     if (options.drawContacts) drawContacts()
@@ -25,39 +27,39 @@ export function bodyDebugger(manager, renderer, options = {}) {
 
     for (let i = 0; i < bodies.length; i++) {
       for (let j = 0; j < bodies[i].length; j++) {
-        drawShapes(bodies[i][j].shape, renderer.ctx)
+        drawShapes(bodies[i][j].shape, ctx)
       }
     }
 
     function drawContacts() {
       for (let i = 0; i < clmd.length; i++) {
         let [p1, p2] = clmd[i].contactData.contactPoints
-        renderer.ctx.beginPath()
-        circle(renderer.ctx, p1.x, p1.y, 5)
-        if (clmd[i].contactData.contactNo === 2) circle(renderer.ctx, p2.x, p2.y, 5)
-        renderer.ctx.closePath()
-        fill(renderer.ctx, "white")
+        ctx.beginPath()
+        circle(ctx, p1.x, p1.y, 5)
+        if (clmd[i].contactData.contactNo === 2) circle(ctx, p2.x, p2.y, 5)
+        ctx.closePath()
+        fill(ctx, "white")
       }
     }
 
     function drawPositions() {
       for (let i = 0; i < transform.length; i++) {
         for (let j = 0; j < transform[i].length; j++) {
-          renderer.ctx.beginPath()
+          ctx.beginPath()
           circle(
-            renderer.ctx,
+            ctx,
             transform[i][j].position.x,
             transform[i][j].position.y,
             2
           )
-          renderer.ctx.closePath()
-          fill(renderer.ctx, "white")
+          ctx.closePath()
+          fill(ctx, "white")
         }
       }
     }
 
     function drawArms() {
-      renderer.ctx.beginPath()
+      ctx.beginPath()
       for (let i = 0; i < clmd.length; i++) {
         const manifold = clmd[i]
         const { contactData: { contactNo, contactPoints } } = clmd[i]
@@ -65,24 +67,24 @@ export function bodyDebugger(manager, renderer, options = {}) {
         const [transformB] = manager.get(manifold.entityB, "transform")
 
         for (let j = 0; j < contactNo; j++) {
-          drawArmRaw(renderer.ctx, transformA.position, contactPoints[j])
-          drawArmRaw(renderer.ctx, transformB.position, contactPoints[j])
+          drawArmRaw(ctx, transformA.position, contactPoints[j])
+          drawArmRaw(ctx, transformB.position, contactPoints[j])
         }
 
       }
-      renderer.ctx.strokeStyle = "yellow"
-      renderer.ctx.stroke()
-      renderer.ctx.closePath()
+      ctx.strokeStyle = "yellow"
+      ctx.stroke()
+      ctx.closePath()
     }
 
     function drawBounds() {
       for (let i = 0; i < bounds.length; i++) {
         for (let j = 0; j < bounds[i].length; j++) {
           renderObj(
-            renderer.ctx,
+            ctx,
             bounds[i][j]
           )
-          stroke(renderer.ctx, "red", 3)
+          stroke(ctx, "red", 3)
         }
       }
     }
@@ -162,5 +164,5 @@ function renderObj(ctx, bounds) {
  * @property {boolean} [drawVelocity=false]
  * @property {boolean} [clearRenderer=false]
  * @property {boolean} [drawCollisionArm=false]
- * @property {boolean} drawContacts
+ * @property {boolean} [drawContacts]
  */
