b3329b025194d5ed96a2dfe1e49c86a6f22a9de4
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-03-19 15:48:34 +0300

    [render] Added `Viewport()` in order to manage html canvas #new

--- src/render/renderers/renderer.js
+++ src/render/renderers/renderer.js
@@ -9,21 +9,54 @@ class Viewport {
   domElement
   constructor(options = {}) {
     this.domElement = options.canvas || document.createElement("canvas")
+    if (!this.domElement.parentElement)
+      Viewport.bindTo(this, document.body)
+    Viewport.set(
+      this,
+      options.width ?? 250,
+      options.height ?? 250
+    )
   }
   /**
    * Attaches the renderer to a given html element by its selector.
    * @param {HTMLElement} element A css selector string that is passed to document.querySelector
    * @param {boolean} focus whether to shift focus of input to the element pr not
-   * @param {Renderer} renderer
+   * @param {Viewport} viewport
    */
-  static bindTo(element, selector, focus = true) {
-    renderer.domElement.remove()
-    element.append(renderer.domElement)
+  static bindTo(viewport, element) {
+    viewport.domElement.remove()
+    element.append(viewport.domElement)
+  }
+  /**
+   * Requests fullscreen for the renderer.
+   * 
+   * @param {Viewport} viewport
+   */
+  static requestFullScreen(viewport) {
+    const parent = viewport.domElement.parentElement
+    if (parent) return parent.requestFullscreen()
+    return viewport.domElement.requestFullscreen()
+  }
+  /**
+   * Sets the width and height of the canvas being rendered to.
+   * 
+   * 
+   * @param {number} w Width of the canvas.
+   * @param {number} h Height of the canvas.
+   * @param {Viewport} viewport
+   */
+  static set(viewport, w, h) {
+    const canvas = viewport.domElement
+    canvas.style.width = w + "px"
+    canvas.style.height = h + "px"
+    canvas.width = w * devicePixelRatio
+    canvas.height = h * devicePixelRatio
   }
 }
 /**
  * This is an abstract class from which different types of renderers are implemented.
  * 
+ * @deprecated
  * @abstract
  * @see Renderer2D
  * @see WebGLRenderer
@@ -44,11 +77,16 @@ export class Renderer extends Viewport {
   }
   /**
    * Attaches the renderer to a given html element by its selector.
+   * 
+   * @deprecated
    * @param {HTMLElement} element A css selector string that is passed to document.querySelector
    * @param {boolean} focus whether to shift focus of input to the element pr not
    * @param {Viewport} renderer
    */
   static bindTo(renderer, element, focus = true) {
+    if(typeof element === "string"){
+      element = document.querySelector(element)
+    }
     if (!element) return console.error("could not find container for the canvas.");
     renderer.domElement.remove()
     renderer.domElement.style.backgroundColor = "grey"
@@ -58,6 +96,7 @@ export class Renderer extends Viewport {
   /**
    * Requests fullscreen for the renderer.
    * 
+   * @deprecated
    * @param {Renderer} renderer
    */
   static requestFullScreen(renderer) {
@@ -67,6 +106,8 @@ export class Renderer extends Viewport {
   }
   /**
    * Sets the width and height of the canvas being rendered to.
+   * 
+   * @deprecated
    * @param {number} w Width of the canvas.
    * @param {number} h Height of the canvas.
    * @param {Renderer} renderer
