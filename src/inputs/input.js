742a363087c74e16d92030926544e9f942c34c9b
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-03-29 11:45:30 +0300

    [Input] Added new plugin `InputPlugin()` which contains keyboard,touch and mouse inputs #new

--- src/inputs/input.js
+++ src/inputs/input.js
@@ -1,57 +1,27 @@
-import { Keyboard } from "./keyboard.js";
-import { Mouse } from "./mouse.js";
-import { Touch } from "./touch.js";
+import { Keyboard, KeyboardPlugin } from "./keyboard.js";
+import { Mouse, MousePlugin } from "./mouse.js";
+import { Touch, TouchPlugin } from "./touch.js";
 import { DOMEventHandler } from "../events/index.js"
+import { deprecate } from "../logger/index.js"
+
+export class InputPlugin {
+  register(manager) {
+    manager.setResource("domeventhandler", new DOMEventHandler())
+    manager.registerPlugin(new TouchPlugin())
+    manager.registerPlugin(new MousePlugin())
+    manager.registerPlugin(new KeyboardPlugin())
+  }
+}
 
 /**
+ * 
+ * @deprecated
  * This handles all inputs from the mouse,touch and keyboards.
  * 
  */
 export class Input {
-  /**
-   * This attaches callbacks to the DOM.
-   * 
-   * @type {DOMEventHandler}
-  */
-  DOMEventHandler
-  /**
-   * 
-   * @type {Mouse}
-  */
-  mouse
-  /**
-   * 
-   * @type {Touch}
-  */
-  touch
-  /**
-   * 
-   * @type {Keyboard}
-  */
-  keyboard
-  /**
-   * @param {DOMEventHandler} eventHandler
-  */
   constructor(eventHandler) {
-    this.DOMEventHandler = eventHandler || new DOMEventHandler()
-    this.mouse = new Mouse(this.DOMEventHandler);
-    this.touch = new Touch(this.DOMEventHandler);
-    this.keyboard = new Keyboard(this.DOMEventHandler);
-  }
-  /**
-   * Updates all inputs.
-  */
-  update() {
-    this.mouse.update()
-    this.touch.update()
-  }
-  /**
-   * Remove all bindings to the DOM for all input types.
-  */
-  dispose() {
-    //TODO remove eventlisteners
-    //this.mouse.dispose()
-    //this.keyboard.dispose()
-    //this.touch.dispose()
+    deprecate("Input()","InputPlugin()")
+    throws("Breaking deprecation encountered")
   }
 }
