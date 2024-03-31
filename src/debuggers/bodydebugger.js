6a239b4062e25790b76d1060ddfdfb3881781397
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-03-31 23:31:54 +0300

    [Physics] Deprecated `Shape()` in favor of `Shape2D()` #new

--- src/debuggers/bodydebugger.js
+++ src/debuggers/bodydebugger.js
@@ -1,4 +1,4 @@
-import { Shape } from "../physics/index.js"
+import { Shape2D } from "../physics/index.js"
 import { BoundingBox, Vector2 } from "../math/index.js"
 import { circle, vertices, stroke, fill } from "../render/index.js"
 import { Manager } from "../ecs/index.js"
@@ -53,7 +53,7 @@ function drawShapes(manager) {
   query.each(body => {
     const shape = body.shape
     ctx.beginPath()
-    if (shape.type === Shape.CIRCLE) {
+    if (shape.type === Shape2D.CIRCLE) {
       circle(
         ctx,
         shape.vertices[0].x,
