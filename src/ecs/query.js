9cce5751ba35fee4ed2291f9b4dd1471a3fb15fe
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-04-06 10:37:17 +0300

    [ECS] Added `Query().eachCombination()` to provide a way to loop through each combination of queried components #new

--- src/ecs/query.js
+++ src/ecs/query.js
@@ -26,7 +26,7 @@ export class Query {
     return this.components
   }
   /**
-   * @param {(comp:T)=>void} callback
+   * @param {(EachFunc<T>} callback
    */
   each(callback) {
     const components = new Array(this.number)
@@ -40,9 +40,34 @@ export class Query {
       }
     }
   }
+  /**
+   * @template {Tuple} T
+   * @param {EachCombinationFunc<T>} callback
+   */
+  eachCombination(callback) {
+    const components1 = new Array(this.number)
+    const components2 = new Array(this.number)
+    if (!this.components) return
+    //This... many people will be very upset.
+    for (let j = 0; j < this.components[0].length; j++) {
+      for (let k = 0; k < this.components[0][j].length; k++) {
+        for (let l = j; l < this.components[0].length; l++) {
+          const nextup = l === j ? k + 1 : 0
+          for (let m = nextup; m < this.components[0][l].length; m++) {
+
+            for (let n = 0; n < this.number; n++) {
+              components1[n] = this.components[n][j][k]
+              components2[n] = this.components[n][l][m]
+            }
+            callback(components1, components2)
+          }
+        }
+      }
+    }
+  }
   single() {
     const components = new Array(this.number)
-    
+
     assert(this.components && this.components[0][0] && this.components[0][0][0], `No entity with component types of \"${ this.type.join("\",\"")}\" found on \`Query().single()\``)
 
     for (let i = 0; i < this.number; i++) {
@@ -53,5 +78,15 @@ export class Query {
 }
 
 /**
- * @typedef {import("typescript").TupleType} Tuple
+ * @template {Tuple} T
+ * @callback EachFunc
+ * @param {T} components
+ * @returns {void}
+ */
+/**
+ * @template {Tuple} T
+ * @callback EachCombinationFunc
+ * @param {T} components1
+ * @param {T} components2
+ * @returns {void}
  */
