28184614f4864bb166caa5826410d602de613d7a
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-04-08 21:13:42 +0300

    [Root] Added typedefs for ecs and app modules #internal #change

---
+++ src/typedef/ecs.js
@@ -0,0 +1,14 @@
+/**
+ * @callback SystemFunc
+ * @param {Registry} registry
+ * @returns {void}
+ */
+/**
+ * @typedef Plugin
+ * @property {RegisterFunc} register
+ */
+/**
+ * @typedef RegisterFunc
+ * @param {App} app
+ * @returns {void}
+ */
