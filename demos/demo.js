c937aa96dbbea3bdc3bff86b12ee6afb99f277e2
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-04-08 23:39:25 +0300

    [Demos] update demos #change

--- demos/demo.js
+++ demos/demo.js
@@ -1,11 +1,11 @@
 import {
-  Manager,
+  App,
   Viewport,
+  RAFPlugin,
   Renderer2DPlugin,
   Physics2DPlugin,
-  TweenPlugin,
-  fpsDebugger,
-  bodyDebugger,
+  FPSDebugger,
+  Body2DDebugger,
   Storage,
   LoadManager,
   SoundLoader,
@@ -16,18 +16,12 @@ export const examples = new Map()
 export const loadmanager = new LoadManager()
 export const imageloader = new ImageLoader(loadmanager)
 export const soundloader = new SoundLoader(loadmanager)
-
-export const manager = new Manager()
+export const manager = new App()
 export const viewport = new Viewport()
 
 export function init(selector) {
-
   Viewport.bindTo(viewport, document.querySelector("#can"))
   Viewport.set(viewport, innerWidth, innerHeight * 0.5)
-
-  //viewport.camera.transform.scale.y = -1
-  //viewport.camera.transform.position.y = viewport.width
-
   play(Storage.get("setup"))
   window.onresize = () => {
     Viewport.set(viewport, innerWidth, innerHeight * 0.5)
@@ -38,33 +32,27 @@ export function init(selector) {
   }
 }
 export function play(name) {
-  manager.clear()
+  manager.registry.clear()
   Storage.set("setup", name)
   if (examples.has(name))
-    examples.get(name)(manager)
+    examples.get(name)(manager.registry)
 }
 export function register(n, f) {
   examples.set(n, f)
 }
 
 soundloader.load("assets/hit.mp3")
-///imageloader.load("assets/dust.jpg")
 imageloader.load("assets/warrior.png")
 
-//Tweens
-manager.registerPlugin(new TweenPlugin("tweenVector2"))
-
-//Physics
-manager.registerPlugin(new Physics2DPlugin())
-
-//Renderer
-manager.registerPlugin(new Renderer2DPlugin({
-  viewport
-}))
-
-//debuggers
-fpsDebugger(manager)
-bodyDebugger(manager, {
-  clearRenderer: false,
-  drawCollisionArm: false
-})
+manager
+  .registerPlugin(new RAFPlugin())
+  .registerPlugin(new Physics2DPlugin())
+  .registerPlugin(new Renderer2DPlugin({
+    viewport
+  }))
+  .registerDebugger(new FPSDebugger())
+  .registerDebugger(new Body2DDebugger({
+    clearRenderer: false,
+    drawCollisionArm: false
+  }))
+  .run()
