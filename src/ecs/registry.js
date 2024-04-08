c8285ccf6095033dab4e32f2712e40e57de8c00c
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-04-08 20:28:41 +0300

    [Ecs] Added `Registry()` which will bookkeep the entities,components,systems and resources of an `App()` #new

---
+++ src/ecs/registry.js
@@ -0,0 +1,113 @@
+import { ArchetypeTable } from "../dataStructures/index.js"
+import { EventDispatcher } from "../events/index.js"
+import { Query } from "./query.js"
+
+export class Registry {
+  /**
+   * @private
+   */
+  _table = new ArchetypeTable()
+  /**
+   * 
+   * @private
+   * @type {SystemFunc[]}
+   */
+  _systems = []
+  /**
+   * @private
+   * @type {Record<string,any>}
+   */
+  _resources = {}
+  events = new EventDispatcher()
+  /**
+   * Adds an entity to the manager and initializes it.
+   * @template {Tuple} T
+   * @param {T} components The entity to add
+   */
+  create(components) {
+    const entity = this._table.insert(components)
+    this.events.trigger("add",entity)
+    return entity
+  }
+  /**
+   * @template {Tuple} T
+   * @param {T[]} entities
+   */
+  createMany(entities) {
+    for (let i = 0; i < entities.length; i++) {
+      this.create(entities[i])
+    }
+  }
+  /**
+   * Removes an entity from the registry.
+   * Note that this doesn't destroy the entity, only removes it and its components from the manager.
+   * To destroy the entity,use `Entity.destroy()` method.
+   * 
+   * @param {Entity} entity The entity to remove
+   */
+  remove(entity) {
+    this.events.trigger("remove",entity)
+    this._table.remove(entity)
+  }
+  /**
+   * @template T
+   * @param {Entity} entity
+   * @param { string[]  } compNames
+   * @returns {T}
+   */
+  get(entity, ...compNames) {
+    // @ts-ignore
+    return this._table.get(entity, compNames)
+  }
+  /**
+   * @template T
+   * @param { string[]  } compNames
+   * @returns {Query<T>}
+   */
+  query(...compNames) {
+    //TODO - Maybe cache the query?
+    const query = new Query(...compNames)
+    query.components = this._table.query(compNames)
+    return query
+  }
+  /**
+   * @template T
+   * @param {string} name
+   * @returns {T}
+   */
+  getResource(name) {
+    return this._resources[name]
+  }
+  /**
+   * @template T
+   * @param {string} name
+   * @param {T} resource
+   */
+  setResource(name, resource) {
+    this._resources[name] = resource
+  }
+  /**
+   * This removes all of the entities and components from the manager
+   */
+  clear() {
+    this.events.trigger("clear", this)
+    this._table.clear()
+  }
+  update(dt = 0.016) {
+    const systems = this._systems;
+    //this.setResource("delta", dt)
+    for (let i = 0; i < systems.length; i++) {
+      systems[i](this);
+    }
+  }
+  /**
+   * Used to register a system
+   *
+   * @param {SystemFunc} sys The system to be addad
+   * 
+   */
+  registerSystem(sys) {
+    this._systems.push(sys)
+  }
+}
+
