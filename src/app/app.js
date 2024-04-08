9331a74cecf82547fc85e5b5ef9f326934299bfc
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-04-08 21:12:21 +0300

    [App] Added `App()` which registers `Plugin()`s and systems and sets resources into the `Registry()`

---
+++ src/app/app.js
@@ -0,0 +1,40 @@
+import { Registry } from "../ecs/index.js"
+import { throws } from "../logger/index.js"
+
+export class App {
+  registry = new Registry()
+  events
+  _initialized = false
+  constructor() {
+    this.events = this.registry.events
+  }
+  run() {
+    this.registry.events.trigger("init")
+    return this
+  }
+  /**
+   * @param {Plugin} plugin
+   */
+  registerPlugin(plugin) {
+    plugin.register(this)
+    return this
+  }
+  /**
+   * @param {Plugin} debug
+   */
+  registerDebugger(debug) {
+    return this.registerPlugin(debug)
+  }
+  /**
+   * @param {SystemFunc} system
+   */
+  registerSystem(system) {
+    if (this._initialized) throws("`App().registerSystem()` cannot be called after `App().run()`")
+    this.registry.registerSystem(system)
+    return this
+  }
+  setResource(name, resource) {
+    this.registry.setResource(name, resource)
+    return this
+  }
+}
