95accd944cb8655e66b2a2fea80ffe46bf1794cd
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-05-12 07:39:52 +0300

    [Time] Added `Timer()` which can be used to time actions #new

---
+++ src/time/timer.js
@@ -0,0 +1,43 @@
+export class Timer {
+  /**
+   * @type {TimerMode}
+   */
+  mode
+  /**
+   * @type {number}
+   */
+  elapsed = 0
+  /**
+   * @type {number}
+   */
+  duration = 0
+  /**
+   * @type {boolean}
+   */
+  finished = false
+  constructor(duration,mode = TimerMode.ONCE){
+    this.duration = duration
+    this.mode = mode
+  }
+  /**
+   * @param {Timer} timer
+   * @param {number} dt
+   */
+  static update(timer, dt) {
+    timer.elapsed += dt
+    timer.finished = false
+    if (timer.elapsed >= timer.duration) {
+      timer.finished = true
+      if (timer.mode === TimerMode.REPEAT)
+        timer.elapsed = 0
+    }
+  }
+}
+/**
+ * @readonly
+ * @enum {number}
+ */
+export const TimerMode = {
+  ONCE : 0,
+  REPEAT : 1
+}
