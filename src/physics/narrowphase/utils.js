e7815e9c7f5cd9e5cb12beb44236b955194bfcc0
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-05-18 22:05:08 +0300

    Refactored the `SATNarrowphase2D()` #internal #change

---
+++ src/physics/narrowphase/utils.js
@@ -0,0 +1,64 @@
+import { Body2D } from "../bodies/index.js"
+import { Shape } from "../shapes/index.js"
+/**
+ * @param {Body2D} a
+ * @param {Body2D} b
+ */
+export function canCollide(a, b) {
+  if (!a.inv_mass && !b.inv_mass)
+    return false
+  if (
+    (a.mask.group && b.mask.group) &&
+    a.mask.group == b.mask.group
+  ) return false
+  if (a.mask.layer && b.mask.layer && a.mask.layer !== b.mask.layer)
+    return false
+  if (a.sleeping && b.sleeping) return false
+  return true
+}
+/**
+ * @param { Vector2} position
+ * @param {number} radius
+ * @param { Vector2} point
+ */
+export function circleContains(position, radius, point) {
+  const dx = point.x - position.x,
+    dy = point.y - position.y
+  if (dx * dx + dy * dy > radius * radius)
+    return false
+  return true
+}
+/**
+ * @param { Vector2[]} vertices
+ * @param {Vector2} point 
+ */
+export function verticesContain(vertices, point) {
+  const pointX = point.x,
+    pointY = point.y,
+    length = vertices.length
+  let previous = vertices[length - 1],
+    current
+  if (length < 2) return false
+  for (let i = 0; i < length; i++) {
+    current = vertices[i];
+    if ((pointX - previous.x) * (current.y - previous.y) +
+      (pointY - previous.y) * (previous.x - current.x) < 0) {
+      return false;
+    }
+    previous = current;
+  }
+
+  return true;
+}
+/**
+ * @param {Shape} shape
+ * @param { Vector2} point
+ */
+export function shapeContains(shape, point) {
+  if (shape.type == Shape.CIRCLE)
+    return circleContains(shape.vertices[0], shape.vertices[1].x, point)
+  return verticesContain(shape.vertices, point)
+}
+export function generatePairID(idA,idB){
+  return idA > idB ? idA + " " + idB : idB + " " + idA
+}
