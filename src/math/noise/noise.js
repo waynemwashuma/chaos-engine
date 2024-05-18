664acd1fa998a6076023900f890034b905d2992f
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-05-18 23:00:16 +0300

    [Math] All methods of `Noise` have been made static #change

--- src/math/noise/noise.js
+++ src/math/noise/noise.js
@@ -1,17 +1,10 @@
-import {rand} from "../math.js"
-import {Interpolation} from "../interpolation.js"
+import { Interpolation } from "../interpolation.js"
 export class Noise {
-  /**
-   * @param {number} seed
-   */
-  constructor(seed = rand() * Number.MAX_SAFE_INTEGER) {
-    this.seed = seed
-  }
   /**
    * @param {number} x
    */
-  get1D(x) {
-    x += this.seed
+  static get1D(x, seed) {
+    x += seed
     x = BigInt((x << 13) ^ x)
     x = (x * (x * x * 15731n + 789221n) + 1376312589n)
     x = parseInt(x.toString(2).slice(-31), 2)
@@ -21,18 +14,18 @@ export class Noise {
    * @param {number} x
    * @param {number} y
    */
-  get2D(x, y) {
+  static get2D(x, y, seed) {
     const intX = Math.floor(x)
     const intY = Math.floor(y)
 
     const tx = x - intX
     const ty = y - intY
 
-    const a = this.get1D(intX + intY)
-    const b = this.get1D(intX + 1 + intY)
-
-    const c = this.get1D(intX + intY + 1)
-    const d = this.get1D(intX + 1 + intY + 1)
+    const a = Noise.get1D(intX + intY, seed)
+    const b = Noise.get1D(intX + 1 + intY, seed)
+    const c = Noise.get1D(intX + intY + 1, seed)
+    const d = Noise.get1D(intX + 1 + intY + 1, seed)
+    
     return Interpolation.cosine(
       Interpolation.cosine(a, b, tx),
       Interpolation.cosine(c, d, ty),
