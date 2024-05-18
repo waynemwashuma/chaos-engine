de027f4daf3d95908a39ef346bdbb41c132523d3
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-05-18 22:56:31 +0300

    [Render] Update the `Canvas2DRendererPlugin()`#change #internal

--- src/render-canvas2d/plugin.js
+++ src/render-canvas2d/plugin.js
@@ -1,39 +1,37 @@
 import { Manager } from "../ecs/index.js"
-import { Renderer2D } from "./canvas.js"
-import { Viewport, Sprite, Material } from "../render/index.js"
-import { deprecate } from "../logger/index.js"
+import { Viewport } from "../render/index.js"
+import { assert } from "../logger/index.js"
+import { drawImage } from "./canvas.js"
+import { MaterialType,Canvas2DMaterial } from "./components/index.js"
 export class Canvas2DRendererPlugin {
   constructor(options = {}) {
-    if (!options.viewport)
-      options.viewport = new Viewport()
-
     this.renderer = options.viewport
     this.camera = options.camera
     this.enableSorting = options.enableSorting ?? false
-    if (!options.viewport.domElement.parentElement)
-      document.body.append(options.viewport.domElement)
   }
   /**
    * @param {Manager} manager
    */
   register(manager) {
-    manager.setResource(this.renderer)
-    manager.setResource(this.renderer.domElement.getContext("2d"))
+    const viewport = new Viewport()
+    manager.setResource(viewport)
+    document.body.append(viewport.domElement)
+    manager.setResource(viewport.domElement.getContext("2d"))
     if (this.enableSorting)
-      manager.registerSystem(updateSortedSprites)
+      manager.registerSystem(renderSortedSprites)
     else
-      manager.registerSystem(updateSprites)
+      manager.registerSystem(renderSprites)
   }
 }
 
-function updateSprites(manager) {
-  const [transforms, sprites] = manager.query(["transform", "sprite"]).raw()
+function renderSprites(manager) {
+  const query = manager.query(["transform", "buffergeometry", "canvas2dmaterial"])
   const dt = manager.getResource("delta")
   const viewport = manager.getResource("viewport")
   /**@type {CanvasRenderingContext2D}*/
   const ctx = manager.getResource("canvasrenderingcontext2d")
   const camquery = manager.query(["transform", "camera"]).single()
-  if (!camquery) return
+  assert(camquery,"Please add a camera entity to the scene.")
   const camtransform = camquery[0]
   ctx.clearRect(0, 0, viewport.width, viewport.height)
   ctx.save()
@@ -48,47 +46,55 @@ function updateSprites(manager) {
     camtransform.scale.x,
     camtransform.scale.y
   )
-  for (let i = 0; i < sprites.length; i++) {
-    for (let j = 0; j < sprites[i].length; j++) {
-      Sprite.render(
-        ctx,
-        sprites[i][j],
-        transforms[i][j].position,
-        transforms[i][j].orientation,
-        transforms[i][j].scale,
-        dt
-      )
-    }
-  }
+
+  query.each((transform, geometry,material) => {
+    ctx.save()
+    ctx.beginPath()
+    ctx.translate(transform.position.x, transform.position.y)
+    ctx.rotate(transform.orientation)
+    ctx.scale(transform.scale.x, transform.scale.y)
+    // @ts-ignore
+    renderMaterial(ctx, material, geometry.drawable, dt)
+    ctx.closePath()
+    ctx.restore()
+  })
   ctx.restore()
 }
 
-function updateSortedSprites(manager) {
-  const [transform, sprite] = manager.query(["transform", "sprite"]).raw()
-  const transforms = transform.slice().flat()
-  const sprites = sprite.slice().flat()
+function renderSortedSprites(manager) {
+  const [transform, geometry, material] = manager.query(["transform", "buffergeometry", "canvas2dmaterial"]).raw()
+  const tra = transform.slice().flat()
+  const geo = geometry.slice().flat()
+  const mat = material.slice().flat()
+
   const dt = manager.getResource("delta")
   const viewport = manager.getResource("viewport")
   const ctx = manager.getResource("canvasrenderingcontext2d")
-  const [camtransform, _] = manager.query(["transform", "camera"]).single()
+  
+  const camquery = manager.query(["transform", "camera"]).single()
+  assert(camquery,"Please add a camera entity to the scene.")
+  const camtransform = camquery[0]
   quickSort(
-    sprites,
+    mat,
     (i, j) => {
-      return Material.getUniform(sprites[i].material, "zIndex") <
-        Material.getUniform(sprites[j].material, "zIndex")
+      return Canvas2DMaterial.getUniform(mat[i], "zIndex") <
+        Canvas2DMaterial.getUniform(mat[j], "zIndex")
     },
     (i, j) => {
-      const temp1 = sprite[j]
-      const temp2 = transform[j]
+      const temp1 = mat[j]
+      const temp2 = tra[j]
+      const temp3 = geo[j]
 
-      sprite[j] = sprite[i]
-      sprite[i] = temp1
-      transform[j] = transform[i]
-      transform[i] = temp2
+      mat[j] = mat[i]
+      mat[i] = temp1
+      tra[j] = tra[i]
+      tra[i] = temp2
+      geo[j] = geo[i]
+      geo[i] = temp3
     }
   )
 
-  Renderer2D.clear(ctx, viewport.width, viewport.height)
+  ctx.clearRect(0, 0, viewport.width, viewport.height)
   ctx.rotate(
     camera.transform.orientation
   )
@@ -112,6 +118,68 @@ function updateSortedSprites(manager) {
   }
 }
 
+function renderMaterial(ctx, material, path, dt) {
+  switch (material.type) {
+    case MaterialType.BASIC:
+      renderBasic(ctx, material, path)
+      break;
+    case MaterialType.TEXT:
+      renderText(ctx, material)
+      break;
+    case MaterialType.IMAGE:
+      renderImage(ctx, material)
+      break;
+  }
+}
+
+function renderBasic(ctx, material, path) {
+  const wireframe = Canvas2DMaterial.getUniform(material, 'wireframe')
+  const fill = Canvas2DMaterial.getUniform(material, 'fill')
+  const stroke = Canvas2DMaterial.getUniform(material, 'stroke')
+  ctx.fillStyle = fill
+  ctx.strokeStyle = stroke
+  if (!wireframe)
+    ctx.fill(path)
+  ctx.stroke(path)
+}
+
+function renderText(ctx, material) {
+  const text = Canvas2DMaterial.getUniform(material, 'text')
+  const textAlign = Canvas2DMaterial.getUniform(material, 'center')
+  const font = Canvas2DMaterial.getUniform(material, 'font')
+  const fill = Canvas2DMaterial.getUniform(material, 'fill')
+  const stroke = Canvas2DMaterial.getUniform(material, 'stroke')
+
+  ctx.textAlign = textAlign
+  ctx.fillStyle = fill
+  ctx.strokeStyle = stroke
+  ctx.font = font
+  ctx.fillText(text, 0, 0)
+  ctx.strokeText(text, 0, 0)
+}
+
+function renderImage(ctx, material) {
+  const image = Canvas2DMaterial.getUniform(material, 'image')
+  const width = Canvas2DMaterial.getUniform(material, 'width')
+  const height = Canvas2DMaterial.getUniform(material, 'height')
+  const frameWidth = Canvas2DMaterial.getUniform(material, 'frameWidth')
+  const frameHeight = Canvas2DMaterial.getUniform(material, 'frameHeight')
+  const frameX = Canvas2DMaterial.getUniform(material, 'frameX')
+  const frameY = Canvas2DMaterial.getUniform(material, 'frameY')
+  drawImage(
+    ctx,
+    image,
+    -width / 2,
+    -height / 2,
+    frameWidth,
+    frameHeight,
+    frameX,
+    frameY,
+    width,
+    height
+  )
+}
+
 /**
  * @template T
  * @param {T[]} arr The array to sort.
@@ -132,12 +200,6 @@ function quickSort(arr, compareFunc, swapFunc, min = 0, max = arr.length - 1) {
     swapFunc(i + 1, max)
     quickSort(arr, compareFunc, swapFunc, min, i)
     quickSort(arr, compareFunc, swapFunc, i + 2, max)
-  }
-}
-export class Renderer2DPlugin extends Canvas2DRendererPlugin {
-  constructor() {
-    deprecate("Renderer2DPlugin()", "Canvas2DRendererPlugin()")
-    super(...arguments)
   }
 }
 /**
