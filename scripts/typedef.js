87e7ffe504a8960dfb812a066816d84fefb33489
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-05-20 20:04:05 +0300

    [scripts] typedef script now concatinate all files in typedef folders into output files #change

--- scripts/typedef.js
+++ scripts/typedef.js
@@ -1,20 +1,22 @@
-import { readFileSync ,writeFileSync,readdirSync } from "fs";
+import { readFileSync,writeFileSync,readdirSync } from "fs";
 import { join,resolve } from "path";
 
-let inputDir1 = resolve(process.cwd(),"src","typedef")
-let inputDir2 = resolve(process.cwd(),"dist")
-let files = readdirSync(inputDir1)
-let outputfiles = readdirSync(inputDir2)
+const inputDir1 = resolve(process.cwd(),"src")
+const inputDir2 = resolve(process.cwd(),"dist")
+const files = readdirSync(inputDir1,{recursive:true})
+    .filter(str => str.includes("typedef"))
+    .filter(path => path.includes(".js"))
+const outputfiles = readdirSync(inputDir2)
 
 let blob = ""
 for (const file of files) {
-    blob += readFileSync(join(inputDir1,file),{encoding:"utf-8"})
+    blob += readFileSync(join(inputDir1,file.toString()),{ encoding: "utf-8" }) + "\n"
 }
 
 for (const filename of outputfiles) {
-    if(filename.includes('.d.ts') || filename.includes('.min.'))continue
+    if (filename.includes('.d.ts') || filename.includes('.min.')) continue
 
-    let output = readFileSync(join(inputDir2,filename),{encoding:"utf-8"}) + blob
+    const output = readFileSync(join(inputDir2,filename),{ encoding: "utf-8" }) + blob
 
     writeFileSync(join(inputDir2,filename),output)
 }
