3eb824a7a345a3ac8f475d3d31c6581ac29c132f
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-06-05 09:00:33 +0300

    [Ecs] Added `Schedule.condition` which implements run condition for each system in a schedule #new

--- src/ecs/schedule/schedule.js
+++ src/ecs/schedule/schedule.js
@@ -1,18 +1,25 @@
+import { Bitset } from "../../dataStructures/index.js"
 export class Schedule {
   systems = []
+  condition = new Bitset()
   /**
    * @param {Function} system
    * @returns {SystemId}
    */
   add(system) {
-    return this.systems.push(system) - 1
+    const length = this.systems.length
+    this.systems.push(system)
+    this.condition.resize(length + 1)
+    this.condition.set(length)
+    
+    return length
   }
   /**
    * @param {Registry} registry
    */
   run(registry) {
     for (let i = 0; i < this.systems.length; i++) {
-      this.systems[i](registry)
+      if(this.condition.get(i))this.systems[i](registry)
     }
   }
 }
