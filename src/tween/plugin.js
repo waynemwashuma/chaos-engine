6dc04c91dc4b848cf3bde3ad48689831f7da010b
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-06-05 08:41:42 +0300

    squash

--- src/tween/plugin.js
+++ src/tween/plugin.js
@@ -1,5 +1,9 @@
 import { throws } from "../logger/index.js"
 import { Angle, Color, Vector2, lerp } from "../math/index.js"
+
+//TweenPlugin<T> where `T: Lerp`
+//e.g TweenPlugin<Position2D>, TweenPlugin<Scale2D> e.t.c...
+//For now,this plugin will be limited to Position,Orientation and Scale (2d and 3d variants) components.
 /**
  * 
  */
@@ -9,16 +13,59 @@ export class TweenPlugin {
    */
   register(manager) {
     manager.registerSystem(updatePosition2DTween)
-    manager.registerSystem(flipPosition2dTween)
-    manager.registerSystem(repeatPosition2dTween)
-    manager.registerSystem(updateTimerPosition2DTween)
+    generateTweenedFn(manager, "position2dtween")
+    manager.registerSystem(updateOrientation2DTween)
+    generateTweenedFn(manager, "orientation2dtween")
+    manager.registerSystem(updateScale2DTween)
+    generateTweenedFn(manager, "scale2dtween")
+    manager.registerSystem(updatePosition3DTween)
+    generateTweenedFn(manager, "position3dtween")
+    manager.registerSystem(updateOrientation3DTween)
+    generateTweenedFn(manager, "orientation3dtween")
+    manager.registerSystem(updateScale3DTween)
+    generateTweenedFn(manager, "scale3dtween")
   }
 }
 
 //the difference between updating systems is how they are lerped.
 //In another compiled language,these could be one templated function.
 
+function generateTweenedFn(manager, name) {
+  //`repeatTween<T>` where `T => Tween<T>`.
+  function repeatTween(manager) {
+    const query = manager.query([name, "tweenrepeat"])
+    query.each(([tween, _]) => {
+      if (tween.timeTaken >= tween.duration) {
+        tween.timeTaken = 0
+      }
 
+    })
+  }
+
+  //`flipTween<T>` where `T => Tween<T>`
+  function flipTween(manager) {
+    const query = manager.query([name, "tweenflip"])
+    query.each(([tween, _]) => {
+      if (tween.timeTaken >= tween.duration) {
+        const temp = tween.to
+        tween.to = tween.from
+        tween.from = temp
+      }
+    })
+  }
+
+  function updateTimerTween(manager) {
+    const dt = manager.getResource("virtualclock").delta
+    const query = manager.query([name])
+
+    query.each(([tween]) => {
+      tween.timeTaken = Math.min(tween.timeTaken + dt, tween.duration)
+    })
+  }
+  manager.registerSystem(flipTween)
+  manager.registerSystem(repeatTween)
+  manager.registerSystem(updateTimerTween)
+}
 //`updateTween<T>` where `T : Lerp` and `T => Tween<T>`.
 //`Lerp` is an interface/trait implementing the lerp method.
 /**
@@ -36,38 +83,63 @@ function updatePosition2DTween(manager) {
   })
 }
 
-//`repeatTween<T>` where `T => Tween<T>`.
-function repeatPosition2dTween(manager) {
-  const query = manager.query(["position2dtween", "tweenrepeat"])
-  query.each(([tween, _]) => {
-    if (tween.timeTaken >= tween.duration) {
-      tween.timeTaken = 0
-    }
+function updateOrientation2DTween(manager) {
+  const query = manager.query(["position2d", "orientation2dtween"])
 
+  query.each(([orientation, tween]) => {
+    const t = tween.easing(
+      tween.timeTaken / tween.duration
+    )
+    orientation.value = lerp(tween.from.value, tween.to.value, t)
   })
 }
 
-//`flipTween<T>` where `T => Tween<T>`
-function flipPosition2dTween(manager) {
-  const query = manager.query(["position2dtween", "tweenflip"])
-  query.each(([tween, _]) => {
-    if (tween.timeTaken >= tween.duration) {
-      const temp = tween.to
-      tween.to = tween.from
-      tween.from = temp
-    }
+function updateScale2DTween(manager) {
+  const query = manager.query(["scale2d", "scale2dtween"])
+
+  query.each(([scale, tween]) => {
+    const t = tween.easing(
+      tween.timeTaken / tween.duration
+    )
+    scale.x = lerp(tween.from.x, tween.to.x, t)
+    scale.y = lerp(tween.from.y, tween.to.y, t)
   })
 }
 
-function updateTimerPosition2DTween(manager) {
-  const dt = manager.getResource("virtualclock").delta
-  const query = manager.query(["position2dtween"])
-  
-  query.each(([tween]) => {
-    tween.timeTaken = Math.min(tween.timeTaken + dt, tween.duration)
+function updatePosition3DTween(manager) {
+  const query = manager.query(["position3d", "position3dtween"])
+
+  query.each(([position, tween]) => {
+    const t = tween.easing(
+      tween.timeTaken / tween.duration
+    )
+    position.x = lerp(tween.from.x, tween.to.x, t)
+    position.y = lerp(tween.from.y, tween.to.y, t)
+    position.z = lerp(tween.from.z, tween.to.z, t)
   })
 }
-/**
- * @typedef TweenPluginOptions
- * @property {string} name
- */
+function updateOrientation3DTween(manager) {
+  const query = manager.query(["orientation3d", "orientation3dtween"])
+
+  query.each(([orientation, tween]) => {
+    const t = tween.easing(
+      tween.timeTaken / tween.duration
+    )
+    orientation.x = lerp(tween.from.x, tween.to.x, t)
+    orientation.y = lerp(tween.from.y, tween.to.y, t)
+    orientation.z = lerp(tween.from.z, tween.to.z, t)
+    orientation.w = lerp(tween.from.w, tween.to.w, t)
+  })
+}
+function updateScale3DTween(manager) {
+  const query = manager.query(["scale3d", "scale3dtween"])
+
+  query.each(([scale, tween]) => {
+    const t = tween.easing(
+      tween.timeTaken / tween.duration
+    )
+    scale.x = lerp(tween.from.x, tween.to.x, t)
+    scale.y = lerp(tween.from.y, tween.to.y, t)
+    scale.z = lerp(tween.from.z, tween.to.z, t)
+  })
+}
