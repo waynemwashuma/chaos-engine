6c6ec362b0f26095d88658a1904c6ae783ffedaf
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-06-11 19:12:15 +0300

    [Math] Removed deprecated methods of `Angle`

--- src/math/angle.js
+++ src/math/angle.js
@@ -1,11 +1,8 @@
-import { deprecate } from "../logger/index.js"
-
 /**
  * Wrapper class since JavaScript doesn't support references to numbers explicitly.
  * Keeps record of the orientation of an entity.
  */
 export class Angle {
-
   /**
    * Orientation in radians.
    * 
@@ -17,31 +14,6 @@ export class Angle {
    */
   constructor(rad = 0) {
     this.value = rad
-  }
-  /**
-   * The orientation in degrees.
-   * @deprecated
-   */
-  set degree(x) {
-    deprecate("Angle.degree")
-    this.value = x * Math.PI / 180
-  }
-  /**
-   * The orientation in radians.
-   * 
-   * @deprecated
-   */
-  set radian(x) {
-    deprecate("Angle().radian", "Angle().value")
-    this.value = x
-  }
-  get radian() {
-    deprecate("Angle().radian", "Angle().value")
-    return this.value
-  }
-  get degree() {
-    deprecate("Angle().degree")
-    return this.value * 180/Math.PI
   }
   /**
    * Copies the orientation of another angle.
