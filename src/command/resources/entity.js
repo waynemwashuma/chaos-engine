6cbb0b023dac884e12cb2d12e0690961b429e03f
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-06-20 06:53:47 +0300

    [Command] Added `EntityCommands` to safely spawning and despawning entities #new

---
+++ src/command/resources/entity.js
@@ -0,0 +1,68 @@
+import { CommandQueue } from "./queue.js"
+import {
+  SpawnCommand,
+  DespawnCommand
+} from "../commands/index.js"
+import { assert } from "../../logger/index.js"
+
+const entityerror = "Spawn an entity using `Entity.spawn()` before using "
+
+export class EntityCommands {
+  /**
+   * @type {CommandQueue<SpawnCommand>}
+   */
+  spawnqueue = new CommandQueue()
+  /**
+   * @type {CommandQueue<SpawnCommand>}
+   */
+  despawnqueue = new CommandQueue()
+  /**
+   * @type {SpawnCommand | null}
+   */
+  buffered = null
+  constructor(registry) {
+    this.registry = registry
+  }
+  spawn() {
+    const entity = this.registry.create([])
+    this.buffered = new SpawnCommand(entity, [])
+    return this
+  }
+  build() {
+    assert(this.buffered, entityerror + "`EntityCommands.build()`.")
+    this.spawnqueue.add(this.buffered)
+    this.buffered = null
+  }
+  id() {
+    assert(this.buffered, entityerror + "`EntityCommands.id()`.")
+    const entity = this.buffered.entity
+    this.build()
+    return entity
+  }
+  /**
+   * @template T
+   * @param {T} component
+   */
+  insert(component) {
+    assert(this.buffered, entityerror + "`EntityCommands.insert()`.")
+    this.buffered.insert(component)
+    return this
+  }
+  insertPrefab(components) {
+    assert(this.buffered, entityerror + "`EntityCommands.insertPrefab()`.")
+    
+    this.buffered.insertPrefab(components)
+    return this
+  }
+  despawn(entity) {
+    this.despawnqueue.add(new DespawnCommand(entity))
+  }
+  apply(registry) {
+    this.spawnqueue.apply(registry)
+    this.despawnqueue.apply(registry)
+  }
+  clear() {
+    this.spawnqueue.clear()
+    this.despawnqueue.clear()
+  }
+}
