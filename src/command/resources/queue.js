bc28103525dae6e86349b3840fb8f673e1196258
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-06-20 06:49:58 +0300

    [Command] Refactored `CommandQueue` #change

--- src/command/resources/queue.js
+++ src/command/resources/queue.js
@@ -1,4 +1,64 @@
 /**
- * @template T
-*/
-export class CommandQueue extends Array {}
+ * @template {Command} T
+ */
+export class CommandQueue {
+  /**
+   * @private
+   * @type {CommandFn<T>}
+   */
+  command
+  /**
+   * @private
+   * @type {T[]}
+   */
+  queue = []
+  /**
+   * @param {CommandFn<T>} [command]
+   */
+  constructor(commandfn = defaultCommandFn) {
+    this.command = commandfn
+  }
+  /**
+   * @param {T} command
+   */
+  add(command) {
+    this.queue.push(command)
+  }
+  /**
+   * @param {Registry} registry
+   */
+  apply(registry) {
+    for (let i = 0; i < this.queue.length; i++) {
+      this.command(this.queue[i], registry)
+    }
+  }
+  clear() {
+    this.queue.length = 0
+  }
+  /**
+   * @returns {Readonly<T[]>}
+   */
+  getQueue() {
+    return this.queue
+  }
+  /**
+   * @returns {number}
+   */
+  size() {
+    return this.queue.length
+  }
+}
+ 
+ /**
+  * @type {CommandFn}
+ */
+ function defaultCommandFn(command,registry) {
+   command.execute(registry)
+ }
+ 
+ /**
+ * @template {Command} T
+ * @callback CommandFn
+ * @param {T} command
+ * @param {Registry} registry
+ */
