91c7f5d2579c2ebefde4c2bdc5d5fce32bbb168e
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-06-21 07:24:05 +0300

    [Command] `CommandsPlugin` registers `EntityCommands` as a resource instead of `CommandQueue` #change

--- src/command/plugin.js
+++ src/command/plugin.js
@@ -1,23 +1,16 @@
-import { CommandQueue } from "./resources/index.js"
+import { EntityCommands } from "./resources/index.js"
 
 export class CommandsPlugin {
-  register(manager) {
-    manager.setResource(new CommandQueue())
-    manager.registerSystem(executeCommands)
-    manager.registerSystem(clearCommandQueue)
+  register(app) {
+    app
+      .setResource(new EntityCommands(app.registry))
+      .registerUpdateSystem(executeCommands)
   }
 }
 
-function executeCommands(manager) {
-  const queue = manager.getResource("commandqueue")
+function executeCommands(registry) {
+  const commands = registry.getResource("entitycommands")
 
-  for (let i = 0; i < queue.length; i++) {
-    queue[i].execute(manager)
-  }
-}
-
-function clearCommandQueue(manager) {
-  const queue = manager.getResource("commandqueue")
-
-  queue.length = 0
+  commands.apply(registry)
+  commands.clear()
 }
