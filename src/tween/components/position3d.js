717f116be24481bc77bc83a673ecbee6d0b351dd
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-06-21 08:55:45 +0300

    [Tween] Added 3d components of tweens #new

---
+++ src/tween/components/position3d.js
@@ -0,0 +1,6 @@
+import { Tween } from "./tween.js"
+
+/**
+ * @extends {Tween<Position3D>}
+*/
+export class Position3DTween extends Tween {}
---
+++ src/tween/components/scale3d.js
@@ -0,0 +1,6 @@
+import { Tween } from "./tween.js"
+
+/**
+ * @extends {Tween<Scale3D>}
+*/
+export class Scale3DTween extends Tween {}
