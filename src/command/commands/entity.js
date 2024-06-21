c904dcd4a3c3991d807387670946b2868082bfcf
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-06-21 07:11:54 +0300

    edit and reword

--- src/command/commands/entity.js
+++ src/command/commands/entity.js
@@ -1,13 +1,34 @@
 import { Command } from './command.js';
 
 export class SpawnCommand extends Command {
+  /**
+   * @readonly
+   * @type {Entity}
+   */
   entity = 0
+  /**
+   * @type {Any[]}
+   */
   components = []
-  constructor(entity, components) {
+  constructor(entity) {
     super()
     this.entity = entity
-    this.components = components
   }
+  /**
+   * @param {any} component
+   */
+  insert(component) {
+    this.components.push(component)
+  }
+  /**
+   * @param {any[]} components
+   */
+  insertPrefab(components) {
+    this.components.push(...components)
+  }
+  /**
+   * @param {Registry} registry
+   */
   execute(registry) {
     registry.insert(this.entity, this.components)
   }
