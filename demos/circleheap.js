8c15009014b407e3df3d90767d73097e78cd2650
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-06-22 09:42:13 +0300

    squash

---
+++ demos/circleheap.js
@@ -0,0 +1,37 @@
+import {
+  BoundingBox,
+  Shape2D,
+  createTransform2D,
+  createMovable2D,
+  createRawRigidBody2D
+} from "/src/index.js"
+import { makePlatform } from "./utils.js"
+
+export function circleheap(manager) {
+  const viewport = manager.getResource("viewport")
+
+  makePlatform(
+    manager,
+    viewport.width / 2,
+    viewport.height * 0.8,
+    viewport.width,
+    50
+  )
+  const height = 10
+  for (let x = 0; x < 10; x++) {
+    stack(200 + x * 50, -100, 50, 50, height, 5, manager)
+  }
+
+  manager.getResource("gravity").y = 900
+}
+
+function stack(x, y, w, h, no, spacing, manager) {
+  for (let i = 0; i < no; i++) {
+    manager.create([
+    ...createTransform2D(x, y + (h + spacing) * i),
+    ...createMovable2D(),
+    new BoundingBox(),
+    ...createRawRigidBody2D(Shape2D.circle(w/2))
+    ])
+  }
+}
