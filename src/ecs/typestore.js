e314a9037e34d5751ded8ab501f8399bcaf22bee
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-06-28 11:45:14 +0300

    [Ecs] Added `TypeStore` which stores component information #new

--- src/ecs/typestore.js
+++ src/ecs/typestore.js
@@ -18,3 +18,43 @@ export class ComponentInfo {
     this.name = name
   }
 }
+
+export class TypeStore {
+  /**
+   * @type {Map<string,ComponentId>}
+   */
+  map = new Map()
+  /**
+   * @type {Array<ComponentInfo>}
+   */
+  list = []
+  /**
+   * @param {Function} componentClass
+   */
+  set(componentClass) {
+    const id = this.list.length
+    const name = componentClass.name.toLowerCase()
+    this.map.set(name, id)
+    this.list.push(new ComponentInfo(id, name))
+
+    return id
+  }
+  /**
+   * @param {string} name
+   */
+  get(name) {
+    return this.getById(this.getId(name))
+  }
+  /**
+   * @param {number} id
+   */
+  getById(id) {
+    return this.list[id]
+  }
+  /**
+   * @param {string} name
+   */
+  getId(name) {
+    return this.map.get(name)
+  }
+}
