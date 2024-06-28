44fe6d41d30b754f9158709936b09a43fd7d073f
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2024-06-28 10:42:09 +0300

    [Ecs] Refactored `Schedule` #internal #change

--- src/ecs/schedule/schedule.js
+++ src/ecs/schedule/schedule.js
@@ -1,7 +1,41 @@
+import { throws } from "../../logger/index.js"
 import { Bitset } from "../../dataStructures/index.js"
+import {
+  Executor,
+  ImmediateExecutor,
+  RAFExecutor,
+  TimeoutExecutor,
+  IntervalExecutor
+} from './executors/index.js'
+
 export class Schedule {
   systems = []
   condition = new Bitset()
+  /**
+   * @type {Executor}
+   */
+  executor
+  /**
+   * @param {ExecutorKind} kind
+   */
+  constructor(registry, kind, time) {
+    switch (kind) {
+      case ExecutorKind.Immediate:
+        this.executor = new ImmediateExecutor(this, registry, time)
+        break
+      case ExecutorKind.Raf:
+        this.executor = new RAFExecutor(this, registry, time)
+        break
+      case ExecutorKind.Timeout:
+        this.executor = new TimeoutExecutor(this, registry, time)
+        break
+      case ExecutorKind.Interval:
+        this.executor = new IntervalExecutor(this, registry, time)
+        break
+      default:
+        throws("No such executor.Use enum `ExecutorKind` to determine which executor to use")
+    }
+  }
   /**
    * @param {Function} system
    * @returns {SystemId}
@@ -11,7 +45,7 @@ export class Schedule {
     this.systems.push(system)
     this.condition.resize(length + 1)
     this.condition.set(length)
-    
+
     return length
   }
   /**
@@ -19,7 +53,23 @@ export class Schedule {
    */
   run(registry) {
     for (let i = 0; i < this.systems.length; i++) {
-      if(this.condition.get(i))this.systems[i](registry)
+      if (this.condition.get(i)) this.systems[i](registry)
     }
   }
+  start() {
+    this.executor.start()
+  }
+  stop() {
+    this.executor.stop()
+  }
+}
+/**
+ * @readonly
+ * @enum {number}
+ */
+export const ExecutorKind = {
+  Immediate: 0,
+  Raf: 1,
+  Timeout: 2,
+  Interval: 3
 }
